9bd67dabdf3225366de814c098b5bba2ac2547c3
Yuga Ikeya <q0014yuga877@icloud.com>
2022-06-17 16:35:34 +0900

    2nd

--- src/spear.c
+++ src/spear.c
@@ -4,7 +4,7 @@ int n;
 int k;
 int A[100000];
 
-int p(long long int m){
+int p(int m){
   int s=0;
   int i=0;
   for(i=0;i<n;i++){
@@ -14,26 +14,25 @@ int p(long long int m){
 }
 
 int main(){
-  int i;
-  long long int  lb, ub;/*ubの初期値を(1e14)+1としたため、long long intを使用*/
+  int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
 
-lb = 0;/*n=1,A[0]=1,槍を一本も作れない場合もある*/
-ub = (1e14)+1; /*n=1e5,k=1,任意のiについてA[i]=1e9の時、p(1e14)=1であるが、p(1e14+1)=0である*/
+lb = 0;/*n=1,k=2,A[0]=1など,槍を1m以上作れない場合もある*/
+ub = (1e9)+1; /*k=1,任意のiについてA[i]=1e9の時、p(1e9)=1であるが、p(1e9+1)=0である*/
       
     while(ub - lb > 1){
-      long long int mid =(lb + ub)/2;
+      int mid =(lb + ub)/2;
         if(p(mid)){
             lb = mid;
         }
         else{
-          ub = mid;
+            ub = mid;
         }
     }
       
-      printf("%lld\n" , lb);
+      printf("%d\n" , lb);
   return 0;
 }
