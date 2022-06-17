9d3a766fcf8ffa35bdcc4bce2eccf45e1d9efced
SuyasuyaNabi <SuyasuyaNabi>
2022-06-17 22:58:56 +0900

    [add] array.cの怪法

--- src/array.c
+++ src/array.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 
-int n;
-int k;
-int A[100000];
+int main()
+{
+  int n;
+  int k;
 
-
-int main(){
-  int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  int A[n];
+  for (int i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
 
+  if (A[n - 1] < k)
+  {
+    printf("%d\n", n);
+    return 0;
+  }
 
+  int lb = 0, ub = n - 1;
+  while (ub - lb > 1)
+  {
+    int cb = (ub + lb) / 2;
+    if (A[cb] >= k)
+    {
+      ub = cb;
+    }
+    else
+    {
+      lb = cb;
+    }
+  }
 
+  printf("%d\n", ub);
   return 0;
 }
