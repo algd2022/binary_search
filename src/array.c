d59a095239e2b8cb9a7a96c97d3c46486cb46abf
Yuga Ikeya <q0014yuga877@icloud.com>
2022-06-17 13:37:15 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -11,6 +11,22 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = -1; /*A[0] >= kの場合も存在するため、初期値lb=-1*/
+  ub = n;  /*与えられた問題文より、A[n]>=k は常に成立すると見做せる。*/
+      
+    while(ub - lb > 1){
+      int mid =(lb + ub)/2;
+      if(A[mid] >= k){
+        ub = mid;
+      }
+      else{
+        lb = mid;
+      }    
+    }
+      
+      printf("%d\n" , ub);
+  
+      return 0;
 
 
 
