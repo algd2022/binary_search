013cb4c0c39727a266cf58769bfe5aef31c6e745
Tomoyuki Nakasuji <nakasuji.t.aa@m.titech.ac.jp>
2022-06-17 20:48:29 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -5,13 +5,36 @@ int k;
 int A[100000];
 
 
+int p(int m){
+  int i;
+  int j = 0;
+  for(i = 0; i < n; i++){
+      if(j >= k){
+        return 0;
+      }else{
+          if(A[i] % m == 0) j = j + (A[i] / m);
+          else j = j + 1 + (A[i] / m);
+      }
+  }
+  return j <= k;
+}
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    lb = 0;
+    ub = 1000000000;
+    while(ub - lb > 1){
+        int mid = (lb + ub) / 2;
+        if (p(mid)){
+            ub = mid;
+        }else{
+            lb = mid;
+        }
+    }
+    printf("%d\n", ub);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -5,13 +5,40 @@ int k;
 int A[100000];
 
 
+int p(int m){
+  int i;
+  int j = 1;
+  int l = m;
+  for(i = 0; i < n; i++){
+    if(m < A[i] || j > k){
+      return 0;
+    }else if(A[i] > l){
+      l = m - A[i];
+      j = j + 1;
+    }else{
+      l = l - A[i];
+    }
+  }
+  if(j > k) return 0;
+  else return 1;
+}
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    lb = 0;
+    ub = 1000000000;
+    while(ub - lb > 1){
+        int mid = (lb + ub) / 2;
+        if (p(mid)){
+            ub = mid;
+        }else{
+            lb = mid;
+        }
+    }
+    printf("%d\n", ub);
   return 0;
 }
