c22e9819a1e310b5e439bb4fa215f3e7edae312b
Yuga Ikeya <q0014yuga877@icloud.com>
2022-06-17 13:59:52 +0900

    1st

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int A[],int m){
+  int s = 0;
+  int i = 0;
+  for(i = 0; i<n; i++){
+    s += (A[i] + m - 1)/m ; /*袋の数は、収穫したリンゴの数を袋に入るリンゴの数で割って切り上げたものである*/
+  }
+  return s <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +19,24 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  
+      
+    lb = 0;         /*袋に入るリンゴが0の時、入力制約から常にp(A,0)=0*/
+    ub = 1e9;/*最悪の場合、n=k,任意のiでA[i]=1e9であるから、xは高々1e9である。*/
+      
+    while(ub - lb > 1){
+      int mid =(lb + ub)/2;
+
+      if(p(A,mid)){
+        ub = mid;
+      }
+
+      else{
+        lb = mid;
+      }
+    }
+      
+    printf("%d\n" , ub);
 
 
   return 0;
