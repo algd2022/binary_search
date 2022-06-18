05ffe25eea36ccbb3d7db1da4b9c3a67913559f7
yuuma0207 <yuuma0207.sabu@gmail.com>
2022-06-18 15:32:48 +0900

    apple made

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,38 @@ int n;
 int k;
 int A[100000];
 
+int p(int x)
+{
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum = sum + (A[i] + x - 1) / x;
+  }
+  return (long long int)sum <= k;
+}
 
-int main(){
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while (ub - lb > 1)
+  {
+    int mid = (ub + lb) / 2;
+    if (p(mid))
+    {
+      ub = mid;
+    }
+    else
+    {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
--- src/array.c
+++ src/array.c
@@ -4,15 +4,29 @@ int n;
 int k;
 int A[100000];
 
-
-int main(){
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
-
-
+  lb = -1;
+  ub = n;
+  while (ub - lb > 1)
+  {
+    int mid = (ub + lb) / 2;
+    if (A[mid] < k)
+    {
+      lb = mid;
+    }
+    else
+    {
+      ub = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
