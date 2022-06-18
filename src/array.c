e2ba7fdda61330f4d54258d997360beb46d81daf
Inose Shota <inoinosecond@gmail.com>
2022-06-18 10:21:57 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -5,14 +5,37 @@ int k;
 int A[100000];
 
 
+int isOK(int key){
+  if (A[key] >= k){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
 
+  lb = -1; // ng
+  ub = n; // ok
 
-
+  while (ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if (isOK(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
+  
   return 0;
 }
