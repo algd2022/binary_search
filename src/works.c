2a0938cd062c8cb9a1886401f78e82a9843a2636
Inose Shota <inoinosecond@gmail.com>
2022-06-18 10:23:38 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,28 @@ int n;
 int k;
 int A[100000];
 
+int isOK(int key){
+  int tem = 0, num = 1; // work time, required worker's num
+
+  for (int i = 0; i < n; i++){
+    if (tem + A[i] > key){
+      if (A[i] > key){//max A > key (cant finish job)
+        return 0;
+      }
+      num += 1;
+      tem = A[i];
+    }
+    else{
+      tem += A[i];
+    }
+  }
+  if (num <= k){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +34,19 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0; // ng
+  ub = 100000000; // ok (10**9)
 
+  while (ub - lb > 1){
+    int mid = (ub + lb) / 2;
+    if (isOK(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
+  
   return 0;
 }
