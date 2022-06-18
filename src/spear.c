cde1da220c4f44c27c1c0f581d2953dbbce3deca
Inose Shota <inoinosecond@gmail.com>
2022-06-18 10:22:27 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,18 @@ int n;
 int k;
 int A[100000];
 
+int isOK(int key){
+  int num = 0; // number of wood bar
+  for (int i = 0; i < n; i++){
+    num += A[i] / key;
+  }
+  if (num >= k){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +24,20 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0; // ok
+  ub = 1000000001; // ng (10**9 + 1)
+
+  while ((ub - lb) > 1){
+    int mid = (ub + lb) / 2;
+    if (isOK(mid)){
+      lb = mid;
+    }
+    else{
+      ub = mid;
+    }
+  }
+
+  printf("%d\n", lb);
 
   return 0;
 }
