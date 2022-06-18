62f33a2eb316d2e4ab80670af792669f080620f8
stjohn2007 <stjohnsgus2007@gmail.com>
2022-06-18 15:01:00 +0900

    revised array.c

--- src/array.c
+++ src/array.c
@@ -4,15 +4,14 @@ int n;
 int k;
 int A[100000];
 
-
-int main(){
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
-
-
 
   return 0;
 }
