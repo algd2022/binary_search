84f365d1eee7daea064c0d5550b7226192b6ddb2
stjohn2007 <stjohnsgus2007@gmail.com>
2022-06-18 16:01:28 +0900

    revised apple.c

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,39 @@ int n;
 int k;
 int A[100000];
 
+int p(int m)
+{
+  int rslt = 0;
+  for (int i = 0; i < n; i++)
+  {
+    rslt += (A[i] + m - 1) / m;
+  }
+  return (long long int)rslt <= k;
+}
 
-int main(){
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
+  ub = 1e9;
+  lb = 0;
 
-
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+    if (p(mid))
+    {
+      ub = mid;
+    }
+    else
+    {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
