6f5894bccd5480c2f01a8c3f82d66d3701b66440
mehm8128 <mehm8128@gmail.com>
2022-06-19 22:18:41 +0900

    solved

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,30 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) {
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += (A[i] + m - 1) / m;
+  };
+  return sum <= k;
+}
 
-int main(){
+int main() {
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++) {
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;  //必ず条件を満たす最小のバッグの容量
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
