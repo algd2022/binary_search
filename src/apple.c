3e6d4df062c55f58422127e3e8c93de9c98b7197
kazuki <kaz.tokyo.tech20@gmail.com>
2022-06-19 15:03:50 +0900

    solve apple.c

--- src/apple.c
+++ src/apple.c
@@ -4,26 +4,42 @@ int n;
 int k;
 int A[100000];
 
+int is_enough_size(int size_of_bag) {
+    int number_of_bag_needed = 0;
+    for (int i = 0; i < n; i++) {
+        if (A[i] % size_of_bag == 0) {
+            number_of_bag_needed += A[i] / size_of_bag;
+        } else {
+            number_of_bag_needed += A[i] / size_of_bag + 1;
+        }
+    }
+
+	return number_of_bag_needed <= k;
+}
+
 int main() {
     int i, lb, ub;
+    int max_number_of_apple = 0;
+
     scanf("%d%d", &n, &k);
     for (i = 0; i < n; i++) {
         scanf("%d", &A[i]);
+        if (A[i] > max_number_of_apple) {
+            max_number_of_apple = A[i];
+        }
     }
-    lb = -1;
-    ub = n;
-    while ((ub - lb) > 1) {
+
+    lb = 0;
+    ub = max_number_of_apple + 1;
+
+    while (ub - lb > 1) {
         int mid = (lb + ub) / 2;
-        if (A[mid] >= k) {
+        if (is_enough_size(mid)) {
             ub = mid;
         } else {
             lb = mid;
         }
     }
-    if (A[ub] >= k) {
-        printf("%d\n", ub);
-    } else {
-        printf("%d\n", n);
-    }
-    return 0;
+
+    printf("%d\n", ub);
 }
