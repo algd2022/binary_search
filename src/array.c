e904133d96bcfc162475f3fee711db3025d9d000
kazuki <kaz.tokyo.tech20@gmail.com>
2022-06-19 14:31:02 +0900

    format

--- src/array.c
+++ src/array.c
@@ -4,15 +4,12 @@ int n;
 int k;
 int A[100000];
 
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-
-  return 0;
+    return 0;
 }
