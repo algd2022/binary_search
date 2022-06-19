c0af29e920f18e680d3adc6c4bfc5ed1e1179758
kazuki <kaz.tokyo.tech20@gmail.com>
2022-06-19 15:17:57 +0900

    solve spear

--- src/spear.c
+++ src/spear.c
@@ -4,12 +4,37 @@ int n;
 int k;
 int A[100000];
 
+int is_get_spear(int length_of_spear) {
+    int number_of_spear_can_be_made = 0;
+    for (int i = 0; i < n; i++) {
+        number_of_spear_can_be_made += A[i] / length_of_spear;
+    }
+    
+    return number_of_spear_can_be_made >= k;
+}
+
 int main() {
-    int i, lb, ub;
+
+    int max_length_of_spear = 0;
     scanf("%d%d", &n, &k);
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d", &A[i]);
+        if(A[i] > max_length_of_spear) {
+            max_length_of_spear = A[i];
+        }
     }
 
+    int ok = 0;
+    int ng = max_length_of_spear + 1;
+
+    while(ng - ok > 1) {
+        int mid = (ng + ok) / 2;
+        if(is_get_spear(mid)) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    printf("%d\n", ok);
     return 0;
 }
