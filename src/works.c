263f57e787c3443b7ffd9d2c288c18e26ac552c1
Kimitaka Ishiguro <kimitaka.i1330pa7@gmail.com>
2022-06-19 23:31:56 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,14 +4,52 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int i,j;
+  int sum = 0;
+  int count = 0;
+  for(i = 0;i < n;i++){
+	if(A[i] > m){
+	  return 0;
+	}
+  }
+  for(j = 0;j < n;j++){
+	sum += A[j];
+	if(sum > m){
+	  count++;
+	  sum = A[j];
+	  if(sum > m){
+		count++;
+		sum = 0;
+	  }
+	}
+	else if(sum == m){
+	  count++;
+	  sum = 0;
+	}
+  }
+  count++;
+  return count <= k;
+}
 
 int main(){
   int i, lb, ub;
+  lb = 0;
+  ub = 1000000001;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  while ( ub - lb > 1) {
+    int mid = ( lb + ub ) / 2;
+    if(p ( mid )){
+      ub = mid ;
+    }
+    else {
+      lb = mid ;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
