315d776ea1c6b659d534e960326b6e11e1cb6a5d
gj5752 <gj57525116@gmail.com>
2022-06-19 23:54:22 +0900

    assignment1 submit

--- src/array.c
+++ src/array.c
@@ -13,6 +13,19 @@ int main(){
   }
 
 
+  lb = -1;
+  ub = n;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(A[mid] >= k){
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
+
 
   return 0;
 }
