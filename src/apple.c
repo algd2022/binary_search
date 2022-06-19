cb04ddeb2ca8286bb2bd43e2e7729280a63b1234
Ken <abc@ishidainuinoMacBook-Pro.local>
2022-06-19 23:10:55 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,39 @@ int n;
 int k;
 int A[100000];
 
+int p(int mid)
+{
+  int ans = 0;
+  for(int i = 0; i < n; i++)
+    ans += (A[i]+mid-1)/mid;
+
+  return ans <= k;
+}
+
+
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for(i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
 
+  
 
+  lb = 0;//０個だったら絶対に入らない、一人一人少なくて１個りんご収穫するから
+  ub = 1e9;//これは一人がどれぐらいりんごを取っても入れる
+  while(ub -lb >1)
+  {
+    int mid = (lb + ub) /2;
+    if(p(mid))
+      ub = mid;
+    else 
+      lb = mid;
+  }
+
+
+  printf("%d\n", ub);
   return 0;
 }
