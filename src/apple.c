5c79edc7d59847f76be43227aba8647dead53c84
gj5752 <gj57525116@gmail.com>
2022-06-19 23:56:05 +0900

    submit

--- src/apple.c
+++ src/apple.c
@@ -5,12 +5,41 @@ int k;
 int A[100000];
 
 
+int p(int m){
+  int i;
+  int j = 0;
+  for(i = 0; i < n; i++){
+    j = j + (A[i] + m - 1) / m;
+  }
+  if(j <= k){
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+
+
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
 
   return 0;
