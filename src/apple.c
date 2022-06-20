9d3624766e5c40315bce7c0351f20908024818f5
Matsushita <matsushita.h.ad@m.titech.ac.jp>
2022-06-20 13:26:19 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 int n;
 int k;
 int A[100000];
+int p(int mid,int n,int k,int A[]){
+  int sum=0;
+  for(int i=0;i<n;i++){
+      sum += (A[i]+mid-1)/mid;
+  }
+  return sum <= k;
+}
 
 
 int main(){
@@ -11,7 +19,14 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb=0;
+  ub=1000000000+1;
+  while(ub - lb > 1){
+    int mid=(ub+lb)/2;
+    if(p(mid,n,k,A)) ub = mid;
+    else lb = mid;
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
