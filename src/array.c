cf3a821ab6bfca4a2f98c363b86f3dd9d71fc5cc
Yamanaka Haruki <yamanaka.h.ab@m.titech.ac.jp>
2022-06-20 21:55:09 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -6,13 +6,22 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    lb = -1;
+    ub = n;
+    while(ub - lb > 1){
+        unsigned int mid = (lb + ub)/2;
+        if(A[mid] < k){
+            lb = mid;
+        }
+        else {
+            ub = mid;
+        }
+    }
+    printf("\n%d",ub);
+    return 0;
 }
