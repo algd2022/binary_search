1ce8f7b8fbfe421c2a2795e29b7cd3e62051656a
Toshiki Goto <19gttsk214@gmail.com>
2022-06-20 17:44:43 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int bag(int m){
+    int j, bag_count=0;
+    for (j = 0; j < n; j++) {
+        bag_count = bag_count + (A[j] + m - 1) / m; //Ø‚èã‚°
+    }
+    return (long long int) bag_count <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +19,19 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;               //x:æ‚è‚¤‚éÅ¬1 = •Ô‚è’l0
+  ub = 1000000000;      //x:æ‚è‚¤‚éÅ‘å10^9 = •Ô‚è’l1
 
+  while (ub - lb > 1) {
+      int mid = (lb + ub) / 2;
+      if (bag(mid)) {
+          ub = mid;
+      }
+      else {
+          lb = mid;
+      }
+  }
+
+  printf("%d\n", ub);       //ã‚©‚ç—}‚¦‚é
   return 0;
 }
