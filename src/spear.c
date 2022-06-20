9963a272e950d21204d21249dc8f740234818ad8
Toshiki Goto <19gttsk214@gmail.com>
2022-06-20 17:45:18 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int long_spear(int m) {
+    int j, spear = 0;
+    for (j = 0; j < n; j++) {
+        spear = spear + A[j] / m;       //切り下げ
+    }
+    return (long long int) spear >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +19,19 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;               //x:取りうる値の最小0 = 返り値1
+  ub = 1000000001;      //x:取りうる値の最大10^9 = 返り値0
 
+  while (ub - lb > 1) {
+      int mid = (lb + ub) / 2;
+      if (long_spear(mid)) {
+          lb = mid;
+      }
+      else {
+          ub = mid;
+      }
+  }
+
+  printf("%d\n", lb);           //下から抑える
   return 0;
 }
