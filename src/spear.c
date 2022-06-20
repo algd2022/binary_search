68502fadca8415a0dbcf691a0a1826b1e3dcb74f
Yamanaka Haruki <yamanaka.h.ab@m.titech.ac.jp>
2022-06-20 21:56:06 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -6,12 +6,35 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub, num, max, sum;
+    max = 0;
+    sum = 0;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &num);
+        A[i] = num;
+        sum = sum + num;
+        if(num > max){
+            max = num;
+        }
+    }
+    lb = max/k;
+    ub = sum/k + 1;
+    while(ub - lb > 1){
+        int mid = (lb + ub)/2;
+        int spears;
+        int j;
+        spears = 0;
+        for(j = 0; j < n; j++){
+            spears = spears + A[j]/mid;
+        }
+        if(spears > k){
+            lb = mid;
+        }
+        else{
+            ub = mid;
+        }
+    }
+    printf("\n%d",lb);
+    return 0;
 }
