50a2f267936d13ca3b056ca168c0c6c6356bb54f
Toshiki Goto <19gttsk214@gmail.com>
2022-06-20 17:30:39 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -4,7 +4,6 @@ int n;
 int k;
 int A[100000];
 
-
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
@@ -12,7 +11,20 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+	lb = -1;		//x:æ‚è‚¤‚éÅ¬0 = •Ô‚è’l0
+	ub = n;			//x:æ‚è‚¤‚éÅ‘ån = •Ô‚è’l1
 
+	while(ub - lb > 1){
+		int mid = (lb + ub) / 2;
 
+		if (A[mid] >= k){
+			ub = mid;
+		}
+		else{
+			lb = mid;
+		}
+	} 
+	
+	printf("%d\n", ub);		//ã‚©‚ç—}‚¦‚é
   return 0;
 }
