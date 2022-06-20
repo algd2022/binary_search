1b65d2fb66464aa4f3b8fe4768dc7f8b67452ce8
Kosei Kikuchi <kosei3521@gmail.com>
2022-06-21 03:42:11 +0900

    solved 8.4

--- src/works.c
+++ src/works.c
@@ -4,6 +4,19 @@ int n;
 int k;
 int A[100000];
 
+int isOk(int A[], int n, int k, int x){//1人がする仕事量の最大値をx以下にできるか
+  int works = 0;
+  int workers = 1;
+  for(int i = 0; i < n; i++){
+    if(A[i] > x) return 0;//そもそも仕事量xを超える仕事があるのならNG
+    works += A[i];
+    if(works > x){
+      workers += 1;
+      works = A[i];
+    }
+  }
+  return workers <= k;//必要な人数がk人以下ならOK
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +24,16 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1e9 + 1;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(isOk(A, n, k, mid)){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
