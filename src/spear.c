f1ae82f23908c9dc2a7c4212cc44b4ed3f6e4792
Ruka Watanabe <watanabe.r.az@m.titech.ac.jp>
2022-06-21 23:35:03 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,15 +3,34 @@
 int n;
 int k;
 int A[100000];
-
-
+int p(int m){
+    return m<k;
+}
+int sum(int A[],int x){
+    int b=0;
+    for(int i=0;i<n;i++){
+       b+=A[i]/x;
+    }
+    return b;
+}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb=0;
+  ub=1000000001;
+  while ( ub - lb > 1) {
+      int mid = ( lb + ub ) / 2;
+      if( p (sum(A,mid))){
+       ub = mid ;
+       }
+      else {
+       lb = mid ;
+      }
+    }
+  printf("\n%d\n",lb);
 
   return 0;
-}
+  }
