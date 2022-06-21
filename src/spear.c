8c6ae73829baf354f3ca4df6bbb06f65937b884f
21-Ryuu <shiqi1634@gmail.com>
2022-06-21 20:53:47 +0900

    .

--- src/spear.c
+++ src/spear.c
@@ -3,15 +3,31 @@
 int n;
 int k;
 int A[100000];
+int p (int m){// means if they want to create m cm's spear, how many (spear) spear can they create.
+  int spear = 0;
+  for (int i=0;i<n;i++){
+    spear += (A[i])/m;
 
+  }
+  return (spear<k);//if spear<k, means they need reduce m
+}
 
 int main(){
-  int i, lb, ub;
+  int i, lb=0, ub=1000000;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
 
+  while (ub-lb>1){
+      int mid = (ub+lb)/2;
+      if (p(mid)){
+        ub = mid;
+      }else{
+        lb = mid;
+      }
+    }
+    printf("%d\n", lb);
 
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -3,15 +3,47 @@
 int n;
 int k;
 int A[100000];
+int p (int m){// means if they want to create m cm's spear, how many (spear) spear can they create.
+  int singleworktime = 0,people=1;
+  for (int i=0;i<n;i++){
+    if (m-singleworktime>=A[i]){
+      singleworktime+=A[i];
+    }
+    else if (m>=A[i]){
+      singleworktime=A[i];
+      people++;
+    }
+    else{
+      return 0;
+    }
+    
+    }
+    return (people<=k);
+}
+
+  
+ 
+
 
 
 int main(){
-  int i, lb, ub;
+  int i, lb=0, ub=1000000;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
 
+  while (ub-lb>1){
+      int mid = (ub+lb)/2;
+      if (p(mid)){
+        ub = mid;
+      }else{
+        lb = mid;
+      }
+    }
+    printf("%d\n", ub);
 
   return 0;
 }
+
+
