1000ab4d81d6bf5b43576d54515c4d4bbdda5816
Ruka Watanabe <watanabe.r.az@m.titech.ac.jp>
2022-06-21 23:36:29 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -1,17 +1,43 @@
-#include <stdio.h>
 
+#include <stdio.h>
 int n;
 int k;
 int A[100000];
-
-
+int p(int m){
+    int work=0;
+    int people=1;
+    for(int i=0;i<n;i++){
+        if(work+A[i]<=m){
+            work+=A[i];
+        }
+        else if(A[i]<=m){
+            people+=1;
+            work=A[i];
+        }
+        else{
+            return 0;
+        }
+    }
+    return people<=k;
+}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb=0;
+  ub=1000000001;
+  while ( ub - lb > 1) {
+      int mid = ( lb + ub ) / 2;
+      if( p (mid)){
+       ub = mid ;
+       }
+      else {
+       lb = mid ;
+      }
+    }
+  printf("\n%d\n",ub);
 
   return 0;
-}
+  }
