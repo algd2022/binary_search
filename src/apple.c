c26b6a0d61491cca51e2008af3365fcdc828900a
Yuki Kinoshita <kinoshita.y.as@m.titech.ac.jp>
2022-06-21 13:59:38 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -2,7 +2,17 @@
 
 int n;
 int k;
+int x;
 int A[100000];
+
+int p(unsigned int x){
+ int sum;
+ sum = 0;
+ for(int i=0 ; i < n ; i++){
+  sum += (A[i] + x - 1) / x;
+ }
+ return sum <= k;
+}
 
 
 int main(){
@@ -12,6 +22,22 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000;
+
+  while(ub - lb > 1){
+   int mid = (lb + ub) / 2;
+
+   if(p(mid)){
+    ub = mid;
+   }
+   else{
+    lb = mid;
+   }
+  }
+  
+  printf("%d\n" , ub);
 
   return 0;
+
 }
