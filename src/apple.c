18774c6f18136945232b84c2655593249639c06b
Chinanago <tomoking1004@icloud.com>
2022-06-22 01:51:40 +0900

    おわり！

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,42 @@ int n;
 int k;
 int A[100000];
 
+int p(int A[],int x){
+  int sum = 0;
+  for(int i=0;n>i;i++){
+    if(A[i]%x == 0){
+      sum += A[i]/x;
+    }else{
+      sum += (A[i]/x) + 1;
+    }
+  }
+  if(sum>k){
+    return 0;
+  }else{
+    return 1;
+  }
+}
 
 int main(){
-  int i, lb, ub;
+  int i, lb, ub, max_a, total;
+  max_a = 0;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+    if(A[i]>max_a){
+      max_a = A[i];
+    }
   }
-
-
+  lb = 0;
+  ub = max_a;
+  while (ub-lb>1){
+    int mid = (lb+ub)/2;
+    if(p(A, mid)){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,22 @@ int n;
 int k;
 int A[100000];
 
+int p(int A[], int x){ //仕事量がx以下になるようにする  
+  int member = 0;
+  int work = 0;
+  int i=0;
+  while((member<k)&&(i<n)){
+    if(work+A[i]<=x){
+      work += A[i];
+      i++;
+    }else{
+      member++;
+      work = 0;
+    }
+  }
+  //printf("%d %d\n", i, member);
+  return i == n ? 1 : 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +27,16 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1e4;
+  while (ub-lb>1){
+    int mid = (lb+ub)/2;
+    if(p(A, mid)){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
