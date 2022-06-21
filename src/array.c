9fce91be2c19f75ce7ba668e8ae7c25dc3c85dd7
Yuto Makimoto <makimoto.y.aa@m.titech.ac.jp>
2022-06-21 16:14:25 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -5,14 +5,24 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-
-  return 0;
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+    lb = 0;
+    ub = n - 1;
+    int ans;
+    if (A[n - 1] < k)ans = n;
+    else {
+        while (lb != ub) {
+            int cen = (lb + ub) / 2;
+            if (A[cen] < k)lb = cen + 1;
+            else ub = cen;
+        }
+        ans = ub;
+    }
+    printf("%d\n", ans);
+    return 0;
 }
