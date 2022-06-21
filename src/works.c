11822a8e23d0af797d03edce20a1660297b9cf45
Matsushita <matsushita.h.ad@m.titech.ac.jp>
2022-06-22 02:39:26 +0900

    works

--- src/works.c
+++ src/works.c
@@ -4,6 +4,21 @@ int n;
 int k;
 int A[100000];
 
+int p(int mid,int n,int k,int A[]){
+  int w_amount=0,w_sumper=1,i;
+
+  for(i=0;i<n;i++){
+    if(A[i]>mid) return 0;
+    w_amount+=A[i];
+    if(w_amount>mid){
+      w_amount=0;
+      w_sumper++;
+      i--;
+    }
+    }
+    return w_sumper<=k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +26,13 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb=0;
+  ub=1000000000;
+  while(ub-lb>1){
+    int mid=(ub+lb)/2;
+    if(p(mid,n,k,A)) ub=mid;
+    else lb=mid;
+  }
+  printf("%d\n",ub);
   return 0;
 }
