25b2df944c6d2bcfe1104ed0233dd43c5ce40625
Kotaro Yanaka <dy23gh69@gmail.com>
2022-06-23 00:26:42 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(int A[], unsigned int x){
+    int t = 0;
+    int i = 0;
+    for (i = 0; i < n; i++){
+        t += A[i] / x;  /*Ø‚èÌ‚Ä‚ÌŒvZ‚ğ‚µ‚Ä˜a‚ğ‚Æ‚é*/
+    }
+    return t < k;
+
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +21,17 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1e9 + 1;  /*1e9‚Ì’·‚³‚Ì‘„‚Íì‚ê‚é‚Ì‚ÅAì‚ê‚È‚¢‚à‚Ì‚Ì‚¤‚¿Å¬‚Ì’·‚³*/
+
+  while(ub - lb > 1){
+        int mid = (ub + lb)/2;
+
+        if(p(A, mid)) ub = mid;
+        else lb = mid;
+  }
+  printf("%d\n", lb);
 
   return 0;
 }
+
