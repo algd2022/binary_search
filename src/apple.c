fc6398aeb5b04d5374a1f47328515c686b39daf8
Tadaaki Kitahara <kakukk.3542@docomo.ne.jp>
2022-06-22 23:12:40 +0900

    commit

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  unsigned int i, s;
+  s = 0;
+  for(i = 1; i <= n; i++){
+    s = s + (A[i - 1] + m - 1) /m;
+  }
+  return  s <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1e9;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
