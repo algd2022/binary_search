377083f07279cf19dd9ae37c7202230a000b2693
Tadaaki Kitahara <kakukk.3542@docomo.ne.jp>
2022-06-22 23:13:04 +0900

    commit

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  unsigned int i, s;
+  s = 0;
+  for(i = 1; i <= n; i++){
+    s = s + A[i - 1] /m;
+  }
+  return  s >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +19,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1e9;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      lb = mid;
+    }
+    else{
+      ub = mid;
+    }
+  }
+  printf("%d\n", lb);
 
 
   return 0;
