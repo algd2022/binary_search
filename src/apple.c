0de7e74e87af8a2c57ec4eafa1bc177a297225f2
Shieru Otsuka <sierun5000@gmail.com>
2022-06-23 00:22:22 +0900

    apple

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,39 @@ int n;
 int k;
 int A[100000];
 
+/* p determines whether all people can take home an apple when the number of
+ apples allowed in the bag is x */
+int p(int x) {
+    int sum = 0; // the number of bags needed
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+    // calculate sum
+    for (int i = 0; i < n; i++) {
+        sum += (A[i] + x - 1) / x;
+    }
 
+    return sum <= k;
+}
 
-  return 0;
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    lb = 0;
+    ub = 1000000000; // = 10^9, max number of apples per 1 person
+
+    // binery search
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (p(mid)) {
+            ub = mid;
+        } else {
+            lb = mid;
+        }
+    }
+
+    printf("%d\n", ub);
+    return 0;
 }
