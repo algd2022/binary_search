30ca55a233e90948f194a5dbb484e202d6ffe20d
calliope <calliope183@gmail.com>
2022-06-22 17:18:56 +0000

    implement `apple.c`

--- src/apple.c
+++ src/apple.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 
-int n;
-int k;
-int A[100000];
+unsigned int division_round_up(unsigned int a, unsigned int x) {
+  // 切り上げ除算(a/x)
+  return (a + x - 1) / x;
+}
 
+unsigned int n, k;
+unsigned int A[100000];
 
-int main(){
-  int i, lb, ub;
+unsigned int p(unsigned int x) {
+  // リンゴバッグの容量をxとしたときに何個のカバンが必要か
+  // n個の要素の配列A
+  int tmp = 0;
+  for(unsigned int i = 0; i < n; i++) {
+    tmp += division_round_up(A[i], x);
+  }
+  return tmp;
+}
+
+int main() {
+  // 入力
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for(unsigned int i = 0; i < n; i++) {
     scanf("%d", &A[i]);
   }
 
+  int lb, ub;
+  lb = 0;
+  ub = 1000000001;
+  while(ub - lb > 1) {
+    unsigned int mid = (lb + ub) / 2;
+    unsigned int bags = p(mid);
+    if(bags <= k) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+
+  printf("%d\n", ub);
 
   return 0;
 }
