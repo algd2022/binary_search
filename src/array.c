430d706fd715c73e6df0671d4b102c81ea657924
Kohei Nakajima <nakajima.k.at@m.titech.ac.jp>
2022-06-23 23:16:20 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -12,7 +12,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+	lb = -1;
+	ub = n;
 
+	while(ub - lb > 1) {
+		int mid = (ub + lb) / 2;
+		if(A[mid] >= k)
+			ub = mid;
+		else
+			lb = mid;
+	}
+
+	printf("%d", ub);
 
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) {
+	int i, sum = 0;
+	for(i = 0; i < n; i++) {
+		sum += A[i] / m;
+	}
+	return sum >= k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -12,6 +20,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+	lb = 0;
+	ub = 1000000001;
+
+	while(ub - lb > 1) {
+		int mid = (lb + ub) / 2;
+		if(p(mid))
+			lb = mid;
+		else
+			ub = mid;
+	}
+
+	printf("%d", lb);
 
   return 0;
 }
