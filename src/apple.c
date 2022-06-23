e46c791468d190f9504440b58dbfc81783495288
BaleYang <yangjianing0309@yahoo.co.jp>
2022-06-23 21:29:28 +0900

    submit

--- src/apple.c
+++ src/apple.c
@@ -3,15 +3,33 @@
 int n;
 int k;
 int A[100000];
-
-
-int main(){
+int p(int x){
+  int sum = 0;
+  int i;
+  for(i = 0;i < n;i++){
+    sum += (int)(A[i]+x-1)/x;
+  }
+  return(sum<=k);
+}
+int main(){ 
   int i, lb, ub;
+
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1e9;
+  while (ub-lb>1)
+  {
+    int mid = (ub+lb)/2;
+    if (p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -3,7 +3,24 @@
 int n;
 int k;
 int A[100000];
-
+int p(int x){
+  int i, sum, worker, worked;
+  worked = 0;
+  worker = 1;
+  for (i = 0; i < n; i++){
+    worked += A[i];
+    if (A[i] > x){
+      return 0;
+    }
+    else if (worked > x){
+      worker += 1;
+      worked = 0;
+      i--;
+    }
+  }
+  return (worker <= k);
+  
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +28,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1e9;
+  while (ub-lb>1)
+  {
+    int mid = (ub+lb)/2;
+    if (p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
