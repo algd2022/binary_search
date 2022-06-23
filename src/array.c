457e160a4c9024edf1d0f329f70c536767fc4584
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2022-06-23 17:13:34 +0900

    array.cを実装した

--- src/array.c
+++ src/array.c
@@ -4,15 +4,26 @@ int n;
 int k;
 int A[100000];
 
-
-int main(){
+int main() {
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++) {
     scanf("%d", &A[i]);
   }
 
+  lb = -1;
+  ub = n;
 
+  while (ub > lb + 1) {
+    int m = (lb + ub) / 2;
+    if (A[m] < k) {
+      lb = m;
+    } else {
+      ub = m;
+    }
+  }
+
+  printf("%d\n", ub);
 
   return 0;
 }
