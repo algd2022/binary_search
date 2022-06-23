f60780e552827278456bef41d756bb59e7a2b959
Kohei Nakajima <nakajima.k.at@m.titech.ac.jp>
2022-06-24 00:13:03 +0900

    revised works.c

--- src/works.c
+++ src/works.c
@@ -4,6 +4,20 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) {
+	int i, works = 0, sum = 1;
+	for(i = 0; i < n; i++) {
+		if(A[i] > m)
+			return 0;
+
+		if((works += A[i]) > m) {
+			sum++;
+			works = A[i];
+		}
+	}
+
+	return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +26,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+	lb = 0;
+	ub = 10000 * n;
+
+	while(ub - lb > 1) {
+		int mid = (lb + ub) / 2;
+		if(p(mid))
+			ub = mid;
+		else
+			lb = mid;
+	}
+
+	printf("%d", ub);
 
   return 0;
 }
