2aa3ffcd64898c204bb1eb2cb46aeddb0dda8420
ryoya <107528208+owadaryoya@users.noreply.github.com>
2022-06-24 01:06:04 +0900

    revised_1~3

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int sum = 0;
+  int i;
+  for(i = 0; i < n; i++){
+    sum += (A[i] + x - 1) / x;
+  }
+
+  return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +20,19 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+
+  lb = 0; //p(0)=0
+  ub = 1000000000; //p(1e9)=1
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){ //バッグが足りた
+      ub = mid; //もっと小さなx(バックに入れられるリンゴの数)を探す
+    }
+    else{ //バッグが足りなかった
+      lb = mid; //もっと大きなx(バックに入れられるリンゴの数)を探す
+    }
+  }
+  printf("%d\n", ub);
 
 
   return 0;
--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int sum = 0;
+  int i;
+  for(i = 0; i < n; i++){
+    sum += A[i] / x;
+  }
+
+  return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +20,19 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  
+  lb = 0; //p(0)=1
+  ub = 1000000001; //p(1e9+1)=0
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){ //k本作れた
+      lb = mid; //もっと大きなx(槍の長さ)を探す
+    }
+    else{ //k本作れなかった
+      ub = mid; //もっと小さなx(槍の長さ)を探す
+    }
+  }
+  printf("%d\n", lb);
 
 
   return 0;
