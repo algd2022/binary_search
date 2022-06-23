414e332669926065cdc964907e4ddfb5be8e6fc6
Taiki Sato <sato.t.cw@m.titech.ac.jp>
2022-06-23 19:52:59 +0900

    apple

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int m){
+  int j;
+  int S = 0;
+  for(j = 0; j < n; j++){
+    S = S + (A[j] + m - 1) / m;
+  }
+  if(S <= k) return 1;
+  else return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +21,15 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (ub + lb) / 2;
+    if(p(mid)) ub = mid;
+    else lb = mid;
+  }
 
+  printf("%d\n",ub);
+  
   return 0;
 }
