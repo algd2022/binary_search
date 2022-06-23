5e2f33bb367a2de2c65c6bef76d853e227ec36c7
AlexiosSou <107452400+AlexiosSou@users.noreply.github.com>
2022-06-23 13:17:57 +0900

    Add files via upload
    できました。

--- src/array.c
+++ src/array.c
@@ -4,6 +4,9 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    return A[m]>=k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,8 +14,27 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  //-1 is out of range of index of A, so it cannot be answer
 
+  //if all members of A are not smaller than k, then -1+1=0 is the answer
 
+  lb=-1;
+
+  //a[n] is set to be infinite,so a[n] must be greater than k
+
+  //if all members of A are smaller than k,then n-1 is not the answer
+   
+  ub=n;
+  while (ub-lb>1){
+      int mid=(ub+lb)/2;
+      if (p(mid)){
+          ub=mid;
+      }
+      else{
+          lb=mid;
+      }
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -1,17 +1,47 @@
 #include <stdio.h>
+#include <math.h>
 
 int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int s=0;
+    int i;
+    for(i = 1; i <= n; i++){
+        s+=A[i]/m;
+    }
+    return s>=k;
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for(i = 1; i <= n; i++){
     scanf("%d", &A[i]);
   }
 
+  //any amount of spear of length 0 can be made,so feasible
+
+  //if n=1,k=2,A[1]=1,then 1 is infeasible
+
+  lb=0;
+
+  //No spear of length 10^9+1 can be made, so infeasible
+
+  //if n=1,k=1,A[1]=10^9,then 10^9 is feasible
+
+  ub=pow(10,9)+1;
+  while (ub-lb>1){
+      int mid=(ub+lb)/2;
+      if (p(mid)){
+          lb=mid;
+      }
+      else{
+          ub=mid;
+      }
+  }
+  printf("%d\n",lb);
 
   return 0;
 }
