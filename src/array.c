647406be2acfa06484366c90b047fbd9f72de566
YUJI <mail@yuji.ne.jp>
2022-06-24 22:04:26 +0000

    Implement.

--- src/array.c
+++ src/array.c
@@ -4,15 +4,31 @@ int n;
 int k;
 int A[100000];
 
+unsigned int binary_search(int A[], int x, int n)
+{
+  int lb = -1;
+  int ub = n;
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+    if (A[mid] >= x)
+      ub = mid;
+    else
+      lb = mid;
+  }
+  return ub;
+}
 
-int main(){
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
 
-
+  printf("%d\n", binary_search(A, k, n));
 
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,46 @@ int n;
 int k;
 int A[100000];
 
+int p(int x)
+{
+  int k2 = 0;
+  for (int i = 0; i < n; i++)
+  {
+    k2 += (A[i] / x);
+  }
+  return k2 >= k;
+}
 
-int main(){
+unsigned int binary_search(int (*A)(int), int x, int n)
+{
+  int lb = -1;
+  int ub = n;
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+    if (A(mid) >= x)
+      lb = mid;
+    else
+      ub = mid;
+  }
+
+  for (int i = ub - 1;; i++)
+  {
+    if (A(i))
+      return i;
+  }
+}
+
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
 
+  printf("%d\n", binary_search(p, 1, 1000000000));
 
   return 0;
 }
