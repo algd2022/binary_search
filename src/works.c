25be3a6073adf21ccdc4d05498246085141050cf
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-06-24 21:51:04 +0900

    🎉 8.4

--- src/works.c
+++ src/works.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int n;
 int k;
 int A[100000];
+
+bool check(int x) {
+  int need = 0;
+  int now = 0;
+  for (int i = 0; i < n; i++) {
+    if (now + A[i] <= x) {
+      now += A[i];
+    } else {
+      need += 1;
+      now = A[i];
+      if (now > x) {
+        return false;
+      }
+    }
+  }
+  return need + 1 <= k;
+}
 
 
 int main(){
@@ -12,6 +30,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+
+  while (ub - lb > 1) {
+    int mid = (ub + lb) / 2;
+    if (check(mid)) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
