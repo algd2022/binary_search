68559147a55b12c7eb43bffd0dd0feb062398578
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-06-24 21:16:02 +0900

    ✨ 8.1

--- src/array.c
+++ src/array.c
@@ -4,15 +4,33 @@ int n;
 int k;
 int A[100000];
 
-
-int main(){
+int main() {
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++) {
     scanf("%d", &A[i]);
   }
 
+  if (A[n - 1] < k) {
+    printf("%d\n", n);
+    return 0;
+  }
+  if (A[0] >= k) {
+    printf("0\n");
+    return 0;
+  }
 
+  lb = 0;
+  ub = n - 1;
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (A[mid] >= k) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
