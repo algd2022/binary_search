34ccc4a15326c6f114b0dda332c38ae70212a92c
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-06-24 21:25:33 +0900

    ✨ 8.2

--- src/apple.c
+++ src/apple.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int n;
 int k;
 int A[100000];
 
+bool check(int x) {
+  int need = 0;
+  for (int i = 0; i < n; i++) {
+    need += (A[i] - 1) / x + 1;
+  }
+  return need <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +20,21 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 1;
+  ub = 1000000000;
+  if (check(lb)) {
+    printf("%d\n", lb);
+    return 0;
+  }
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (check(mid)) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
