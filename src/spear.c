dd3581fb53363ae1fc8b4b051cd80811160665bd
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-06-24 21:30:41 +0900

    ✨ 8.3

--- src/spear.c
+++ src/spear.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int n;
 int k;
 int A[100000];
+
+bool check(int x) {
+  int can = 0;
+  for (int i = 0; i < n; i++) {
+    can += A[i] / x;
+  }
+  return can >= k;
+}
 
 
 int main(){
@@ -12,6 +21,23 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 1;
+  ub = 1000000001;
+  if (!check(lb)) {
+    printf("%d\n", 0);
+    return 0;
+  }
+
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (check(mid)) {
+      lb = mid;
+    } else {
+      ub = mid;
+    }
+  }
+
+  printf("%d\n", lb);
 
   return 0;
 }
