d6c8000b4110e1f27047d92fd28ab31d5b15e38b
Kosei Oishi <oishi.k.ah@m.titech.ac.jp>
2022-06-24 17:40:21 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int a[], int x){
+  int sum = 0;
+  for (int i = 0; i < n; i++){
+   sum += (a[i] + x - 1) / x;
+  }
+  return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +19,16 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+
+  while (ub - lb > 1) {
+    int mid = (ub + lb) / 2;
+    if (p(A,mid))  ub = mid;
+    else  lb = mid;
+  }
+
+  printf("%d\n",ub);
 
   return 0;
 }
