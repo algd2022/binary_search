79191c0671e4beaf1de1a43dc8a8645361712a12
Kosei Oishi <oishi.k.ah@m.titech.ac.jp>
2022-06-24 17:41:21 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,29 @@ int n;
 int k;
 int A[100000];
 
+// (x時間以下の時間ずつ仕事量を分割したときの分割数) <= k を満たすとき p = 1 とする 
+int p(int a[], int x){
+  // 一番仕事量が多い人の仕事量は　(a[i]の最大値)　時間以上である
+  for (int i = 0; i < n; i++){
+    if (a[i] > x)  return 0; 
+  }
+  
+  // x時間以下の時間ずつ仕事量を分割したときの分割数を求める
+  int count = 0;  // count は (分割数) - 1 を表す
+  int sum = 0;
+  int b[100000];    
+  
+  b[0] = 0;
+
+  for (int i = 0; i < n; i++){
+    sum += a[i];
+    if (sum - b[count] > x){
+      b[++count] = sum - a[i];
+    }
+  }
+
+  return count < k; 
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +35,16 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+
+  while (ub - lb > 1) {
+    int mid = (ub + lb) / 2;
+    if (p(A,mid))  ub = mid;
+    else lb = mid;
+  }
+
+  printf("%d\n", ub);  
 
   return 0;
 }
