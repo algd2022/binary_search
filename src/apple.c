4291ce90a858ceddf683756524315d650152c728
Yuki Wata <yuuki140104@gmail.com>
2022-06-25 16:46:45 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(unsigned int x) {
+  int s = 0;
+  for (int i = 0; i < n; i++) {
+    s += (A[i] - 1) / x + 1;
+  }
+  return s <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +18,16 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000001;
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
