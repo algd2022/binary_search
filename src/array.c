311ed73d65cb23b808d9d76cde3ba79ff7dc9161
Yuki Wata <yuuki140104@gmail.com>
2022-06-25 16:44:28 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -4,6 +4,9 @@ int n;
 int k;
 int A[100000];
 
+int p(unsigned int x) {
+  return A[x] >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,8 +14,24 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  if (A[0] >= k) {
+    printf("0\n");
+    return 0;
+  }
+  if (A[n - 1] < k) {
+    printf("%d\n", n);
+    return 0;
+  }
+  lb = 0;
+  ub = n - 1;
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
