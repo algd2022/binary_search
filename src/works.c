3b9e40e2fc16d693c89300f751147ddc249676a1
Yuki Wata <yuuki140104@gmail.com>
2022-06-25 16:47:25 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,20 @@ int n;
 int k;
 int A[100000];
 
+int p(unsigned int x) {
+  int num_people = 1;
+  int workload = 0;
+  for (int i = 0; i < n; i++) {
+    if (A[i] > x) return 0;
+    if (workload + A[i] <= x) {
+      workload += A[i];
+    } else {
+      num_people += 1;
+      workload = A[i];
+    }
+  }
+  return num_people <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +25,16 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while (ub - lb > 1) {
+    int mid = (ub + lb) / 2;
+    if (p(mid)) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
