29fe7ac195687e6531040d4a9e154a7841b55a29
NagaiDaiki <nagai.d.ab@m.titech.ac.jp>
2022-06-25 10:35:55 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,14 +4,43 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int m){
+    int sigma = 0;
+    int j;
+    int number = 1;
+    for (j =0; j<n; j++){
+      if (A[j]>m){
+      return 0;
+      }
+      else if(sigma + A[j]<= m){
+      sigma  = sigma + A[j];
+      }
+      else{ 
+      sigma = A[j];
+      number = number + 1;
+      }
+      }
+    return number <= k;
+}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = 0;
+  ub = 1000000000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  while(ub - lb > 1){
+	int mid = (lb + ub) / 2;  
+     
+	if( p(mid)) {
+      ub = mid;
+      }
+	else {
+      lb = mid;
+      }
+  }
+  printf ("%d\n" , ub);
 
   return 0;
 }
