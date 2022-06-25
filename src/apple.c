9bde2ff41f2fa0e7b7433ac6f410b26ba2826936
NagaiDaiki <nagai.d.ab@m.titech.ac.jp>
2022-06-25 10:35:03 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,33 @@ int n;
 int k;
 int A[100000];
 
-
+ int p(int m){
+      int sigma = 0;
+	int j;
+      for (j =0; j<n; j++){
+      sigma  = sigma + (A[j] + m -1)/m;
+      }
+      return sigma <= k;
+      }
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = 0;
+  ub = 1000000000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  while(ub - lb > 1){
+	int mid = (lb + ub) / 2;  
+     
+	if( p(mid)) {
+      ub = mid;
+      }
+	else {
+      lb = mid;
+      }
+  }
+  printf ("%d\n" , ub);
 
   return 0;
 }
