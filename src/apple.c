3da2097178675039c18581d3df5cb8e245d77765
Morito Sato <morito.s2001136@gmail.com>
2022-06-25 19:07:05 +0900

    apple.c-revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,24 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int x){
+  int i;
+  int sum = 0;
+  for(i = 0; i < n;i++){
+    sum += (A[i] + x - 1) / x;
+  }
+  return sum <= k;
+}
+
+int my_pow(int base, unsigned int exp) {
+    unsigned int i;
+    int ans;
+    ans = 1;
+    for (i = 0; i < exp; i++) {
+        ans = ans * base;
+    }
+    return ans;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +29,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = my_pow(10,9);//10^9の計算。
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
