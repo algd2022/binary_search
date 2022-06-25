633a8024140ad4d048c5eb79ba64cce47dc7abde
Takeshi Ota <ota.t.ah@m.titech.ac.jp>
2022-06-25 13:44:13 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -6,12 +6,24 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
+  int i, lb, ub, sum = 0;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+    sum = sum + A[i];
   }
-
-
+  lb = -1;
+  ub = n;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    int capa = sum / k;
+    if(A[mid] > capa) {
+        ub = mid;
+    }
+    else {
+        lb = mid;
+    }
+  }
+  printf("%d\n", A[ub]);
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -5,13 +5,28 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
 
-
-  return 0;
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+    lb = 0;
+    ub = 100000000 - 1;
+    while(ub - lb > 1){
+        int num = 0;
+        int mid = (lb + ub + 1) / 2;
+        for(i = 0; i < n; ++i){
+            num += A[i] / mid;
+        }
+        if(k <= num){
+            lb = mid;
+        }
+        else{
+            ub = mid;
+        }
+    }
+    printf("%d\n", lb);
+    return 0;
 }
