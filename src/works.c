b425024c30e1decf10ab6091e2cd5fe08b5e3c5f
Hayato Koriyama <koriyama.h.ab@m.titech.ac.jp>
2022-06-26 00:00:58 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,21 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int j = 0,count=0;
+    while (1) {
+        count++;
+        if (A[j] > x)
+            return 0;
+        int stack = 0;
+        while (stack + A[j] <= x) {
+            stack += A[j];
+            j++;
+            if (j >= n)
+                return count <= k;
+        }
+    }
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +26,15 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = n * 10000;
+  while (ub - lb > 1) {
+      int mid = (ub + lb) / 2;
+      if (p(mid))
+          ub = mid;
+      else
+          lb = mid;
+  }
+  printf("%d\n", ub);
   return 0;
 }
