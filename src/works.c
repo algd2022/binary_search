9a5933ba3c0fd4f58a5752e66425ac8bddf6e376
Morito Sato <morito.s2001136@gmail.com>
2022-06-25 19:08:46 +0900

    works.c-revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,34 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int x){
+  int i;
+  int worklord = 0;
+  int number = 1;
+  for(i = 0; i < n;i++){
+    if(A[i] > x){//仕事量xより大きな仕事量A[i]が存在する
+      return 0;
+    }
+    else if (worklord + A[i] <= x){//仕事を継続する
+      worklord += A[i];
+    }
+    else{//仕事を次の人に受け渡す
+      worklord = A[i];
+      number++;
+    }
+  }
+  return number <= k;
+}
+
+int my_pow(int base, unsigned int exp) {
+    unsigned int i;
+    int ans;
+    ans = 1;
+    for (i = 0; i < exp; i++) {
+        ans = ans * base;
+    }
+    return ans;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +39,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = my_pow(10,9);//10^9の計算。
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
