8fb054f94eeb3761b5cf87ab71634414ffed8bfe
NagaiDaiki <nagai.d.ab@m.titech.ac.jp>
2022-06-25 10:35:40 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,34 @@ int n;
 int k;
 int A[100000];
 
+  int p(int m){
+  	int sigma = 0;
+      int j;
+      for (j=0; j<n; j++){
+      sigma = sigma + A[j]/m;
+      } 
+      return sigma >= k;
+      }
 
 int main(){
   int i, lb, ub;
+  lb = 0;
+  ub = 1000000001;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+   while(ub - lb > 1){
+	int mid = (lb + ub) / 2;  
+     
+	if( p(mid)) {
+      lb = mid;
+      }
+	else {
+      ub = mid;
+      }
+  }
+  printf ("%d\n" , lb);
 
   return 0;
 }
