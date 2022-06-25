378970586980868cef6b4363ce4da16d6fd68c21
Shino Ito <ito.s.cd@m.titech.ac.jp>
2022-06-25 17:12:52 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,17 @@ int n;
 int k;
 int A[100000];
 
+int bagcalc(int maxnum){
+  int i, count=0;
+  for(i=0; i<n; i++){
+    count+=(A[i]+maxnum-1)/maxnum;
+  }
+  if(count<=k){
+    return 1;
+  }else{
+    return 0;
+  }
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +23,16 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-
+  lb = 0;
+  ub = A[0];
+  for(i = 0; i < n; i++){
+    if(ub < A[i]) ub = A[i];
+  }
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(bagcalc(mid)) ub = mid;
+    else lb = mid;
+  }
+  printf("%d\n", ub);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,36 @@ int n;
 int k;
 int A[100000];
 
+int possible(int work){ // calculates how many people will needed if each person work "work"
+  int i, j, countppl;
+  int max = A[0];
+  for(i = 1; i<  n; i++){
+    if(max < A[i]) max = A[i];
+  }
+
+  if(work < max)return 0;
+  else{
+    j = 0;
+    countppl = 1;
+    for(i = 0; i < n; i++){
+      j += A[i];
+      printf("%d %d\n", i, j);
+      if(j == work && i != n-1){
+        j = 0;
+        countppl++;
+      }else if(j > work){
+        j = 0;
+        countppl++;
+        i--;
+      }
+    }
+  }
+  if(countppl <= k){
+    printf("return 1\n");
+   return 1;
+  }
+  else return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +42,30 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = A[0];
+  for(i = 1; i < n; i++){
+    if(lb > A[i]) lb = A[i];
+  }
+  ub = 0;
+  for(i = 0; i < n; i++){
+    ub += A[i];
+  }
+  
+  while(ub - lb > 1){
+    int mid = (ub + lb +1) / 2;
+    printf("mid: %d\n", mid);
+    if(possible(mid)){
+      printf("old ub: %d ", ub);
+      ub = mid;
+      printf("new ub: %d ", ub);
+    }
+    else{
+      lb = mid;
+      printf("failed lb: %d ", lb);
+
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
