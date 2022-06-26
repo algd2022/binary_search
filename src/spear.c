32904eceb8774e48914fd1348743bc8407e117fc
永岡由之 <yoshiyuki.nagaoka.yn@gmail.com>
2022-06-27 00:28:00 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -5,6 +5,24 @@ int k;
 int A[100000];
 
 
+int num(int x){
+  int sum = 0;
+  int i;
+  for(i = 0; i < n; i++){
+    sum += A[i] / x;
+  }
+
+   if (sum >= k){
+
+     return 1; 
+   }
+  
+   else{
+     return 0;
+   }
+} 
+
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
@@ -12,6 +30,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-
+lb = 0;
+ub = 1e9+1; 
+while(ub - lb > 1) {
+  int mid = (ub + lb) / 2;
+   if (num(mid)){
+     lb = mid;
+   }
+   else {
+     ub = mid;
+   }
+ }
+  
+  printf("%d\n", lb);
   return 0;
 }
