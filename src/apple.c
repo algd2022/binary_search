69bb89874ffdccc9237b7ca850dfe8789ccc43c1
Ryomatech <95959694+Ryomatech@users.noreply.github.com>
2022-06-27 00:35:40 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,20 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int x){
+  int i, bagnum;
+  bagnum = 0;
+  for(i = 0;i < n;i++){
+    bagnum += (A[i] + x - 1)  / x ;
+    //mod x > 0で+1, mod x = 0で+0
+  }
+  if(bagnum <= k){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +25,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (ub + lb) / 2;
+    if (p(mid) == 0){
+      lb = mid;
+    }
+    else{
+      ub = mid;
+    }
+  }
+  printf("%d\n" , ub);
   return 0;
 }
