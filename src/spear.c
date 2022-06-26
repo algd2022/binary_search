4816c3288bcc7adcfbf43a4029b1cf5e43ee4c18
kitamuratomohiro <kitamura.t.ae@m.titech.ac.jp>
2022-06-27 01:41:41 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int sum = 0;
+  int i;
+  for(i = 0; i < n; i++){
+    sum = sum + (A[i] / m);
+  }
+  return sum < k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +20,19 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1e9;
+  
+  while(ub - lb > 1){
+    int mid = (ub + lb)/2; 
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
 
+  printf("%d\n", lb);
   return 0;
 }
