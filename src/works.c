419c245d7be641018e398c17ecd368a59dfaae7d
Tomosuke Ichioka <ichioka.t.aa@m.titech.ac.jp>
2022-06-26 20:27:03 +0900

    Submission

--- src/works.c
+++ src/works.c
@@ -4,6 +4,20 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int x){
+  unsigned int sum, count;
+  sum = 0;
+  count = 1;
+  for(int i=0; i<n; i++){
+    sum = sum + A[i];
+    if(sum > x){
+      if(A[i] > x) return 0;
+      sum = A[i];
+      count++;
+    }
+  }
+  return count <= k; 
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +25,14 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)) ub = mid;
+    else lb = mid;
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
