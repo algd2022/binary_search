2a98fed8a1177d2840714807f74cc1eb414ade88
堀内麻帆 <maho@horiuchimahonoMacBook-Pro.local>
2022-06-26 15:52:06 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,21 @@ int n;
 int k;
 int A[100000];
 
+int max(int n, int *A) {
+  int i, max = A[0];
+  for(i = 1; i < n; i++){
+    if(A[i] > max)
+       max = A[i];
+  }
+  return max;
+}
+unsigned int p(int x, int A[]){
+  int i, sum=0;
+  for(i = 0; i < n; i++){
+    sum += A[i] / x;
+  }
+  return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +27,17 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-
+  lb = -1;
+  ub = max(n, A);
+  while(ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid, A)){
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
