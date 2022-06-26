4bd319892c486f63716edd637fc4f2a01b39bbdc
Ryoya Yoshimura <yoshimura.r0511@gmail.com>
2022-06-26 18:48:01 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,13 +4,31 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int m){
+  int S = 0;
+  for (int i = 0; i < n;i++){
+      S +=  A[i] / m;
+    }
+  return S < k;
+  }
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0 ;
+  ub = n*1000000000+1;
+  while(ub - lb>1){
+  int mid = (lb + ub) / 2;
+  if (p(mid)){
+      ub = mid;
+    }
+    else {
+        lb = mid;
+    }
+  }
+  printf("%d\n", lb);
 
 
   return 0;
