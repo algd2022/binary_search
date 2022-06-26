377ba36a835cbec170575cb22c421c430385e5e3
Kouta Kumamoto <kouta.kumamoto@icloud.com>
2022-06-26 19:25:08 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,7 +3,19 @@
 int n;
 int k;
 int A[100000];
-
+int p(int x){
+  int i;
+  int a = 0;
+  for(i = 1; i < n; i++){
+    a = a + A[i] / x;
+  }
+  if(a >= k){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +23,19 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1000000001;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid) == 0){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+printf("%d\n", lb);
+
 
 
   return 0;
