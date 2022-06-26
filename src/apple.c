03b6996879d9aef44cc0a9d33f3356233bf80684
永岡由之 <yoshiyuki.nagaoka.yn@gmail.com>
2022-06-26 23:48:33 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,23 @@ int n;
 int k;
 int A[100000];
 
+int num(int x){
+  int sum = 0;
+  int i;
+  for(i = 0; i < n; i++){
+    sum += (A[i] + x - 1) / x;
+  }
+
+   if (sum <= k){
+
+     return 1; 
+   }
+  
+   else{
+     return 0;
+   }
+} 
+
 
 int main(){
   int i, lb, ub;
@@ -12,6 +29,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-
+lb = 0;
+ub = 1e9; /* maximum number of apple picked up is ub*/
+while(ub - lb > 1) {
+  int mid = (ub + lb) / 2;
+   if (num(mid)){
+     ub = mid;
+   }
+   else {
+     lb = mid;
+   }
+ }
+  
+  printf("%d\n", ub);
   return 0;
 }
