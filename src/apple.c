c4a68e8746ef4b642998de022355c61af17e40dd
grkon <yo.for.user@gmail.com>
2022-06-26 14:48:46 +0900

    complete apple.c

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,43 @@ int n;
 int k;
 int A[100000];
 
+int ceiling(double x)
+{
+  if (x == (int)x)
+  {
+    return x;
+  }
+  else
+  {
+    return ((int)x + 1);
+  }
+}
+
+int max()
+{
+  int max_p = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (A[i] > max_p)
+    {
+      max_p = A[i];
+    }
+  }
+
+  return n * max_p;
+}
+
+int p(int x)
+{
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum += ceiling((double)A[i] / x);
+  }
+
+  return (sum <= k);
+}
+
 int main()
 {
   int i, lb, ub;
@@ -12,6 +49,23 @@ int main()
   {
     scanf("%d", &A[i]);
   }
+
+  lb = 0;
+  ub = max();
+
+  int mid = (ub + lb) / 2;
+
+  while (ub - lb > 1)
+  {
+    if (p(mid))
+      ub = mid;
+    else
+      lb = mid;
+
+    mid = (ub + lb) / 2;
+  }
+
+  printf("%d", ub);
 
   return 0;
 }
