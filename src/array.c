cfc78fc8c514cd0f432bacf0f657d463d0cc484c
hanaka <hanaka@fleuraison.net>
2022-06-27 00:33:40 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -3,7 +3,9 @@
 int n;
 int k;
 int A[100000];
-
+int p(int m){
+  return m >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,8 +13,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = -1;
+  ub = n;
 
-
-
+  while(ub -lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(A[mid])){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,29 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  //sum:1人当たりの仕事量を加算
+  //counts:mを仕事量の上限として必要な人数をカウント
+  int j, sum = 0, counts = 0;
+  for(j = 0; j < n; j++){
+    if(A[j] > m){
+      return 0;
+    }
+    else{
+      sum += A[j];
+      if(sum > m){
+        counts++;
+        sum = A[j];
+      }
+      else if(sum == m){
+        counts++;
+        sum = 0;
+      }
+    }
+  }
+  counts++;
+  return counts <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +35,19 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
 
+  while(ub -lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+
+  printf("%d\n", ub);
   return 0;
 }
