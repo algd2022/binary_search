c936e54fa24245e99cbcb8d5319ade7167c9aa81
Guppi <20153267g@gmail.com>
2022-06-27 00:24:08 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int sum = 0;
+  int i;
+  for(i = 0; i < n; i++){
+    sum += (A[i] + m - 1) / m;
+  }
+  return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +20,19 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 1;
+  ub = 1000000001;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      lb = mid;
+    }
+    else{
+      ub = mid;
+    }
+  }
+
+  printf("%d\n", lb);
 
   return 0;
 }
