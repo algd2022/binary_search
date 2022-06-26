4ad582fd46acf02b12047217099c79a06017f1a0
matsuno yukinobu <matsuno.y.af@m.titech.ac.jp>
2022-06-26 23:06:30 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -3,15 +3,37 @@
 int n;
 int k;
 int A[100000];
-
+int p(int x) {
+  int i, cnt = 0;
+  for (i = 0; i < n; i++) {
+    if(A[i]%x == 0){
+      cnt += A[i] / x;
+    }
+    else {
+      cnt += A[i] / x + 1;
+    }
+  }
+  return cnt<=k;
+}
 
 int main(){
   int i, lb, ub;
+  
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1000000000;
+  while (ub -lb >1){
+    int mid = (ub + lb) / 2;
+    if (p(mid)){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -3,7 +3,21 @@
 int n;
 int k;
 int A[100000];
-
+int p(int x) {
+  int i, cnt = x, man = 1;
+  for (i = 0; i < n; i++) {
+    if(cnt >= A[i]){
+      cnt -= A[i];
+    }else{
+      man++;
+      cnt = x - A[i];
+      if(cnt < 0){
+        return 0;
+      }
+    }
+  }
+  return man <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +25,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1000000000;
+  while (ub - lb > 1) {
+    int mid = (ub + lb) / 2;
+    if (p(mid)) {
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
