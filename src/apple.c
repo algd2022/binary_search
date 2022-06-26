376a3b015a8bfce5313b698492ba6555011a49b7
Ryoya Yoshimura <yoshimura.r0511@gmail.com>
2022-06-26 18:44:42 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,31 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int x){
+  int S = 0;
+  for (int i = 0; i < n;i++){
+      S += (A[i] + x - 1) / x;
+    }
+  return  S <= k;
+  }
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1000000000+1;
+while(ub - lb>1){
+  int mid = (lb + ub) / 2;
+  if (p(mid)){
+      ub = mid;
+    }
+    else {
+        lb = mid;
+    }
+}
+printf("%d\n", ub);
 
   return 0;
 }
