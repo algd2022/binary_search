cdd45ee2e47d64b4ef9984b9b80b07f7a12a0924
Guppi <20153267g@gmail.com>
2022-06-27 00:24:51 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,20 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int j, i = 0;
+  for(j = i; j < k; j++){
+    int x = m - A[i];
+    while(x >= 0){
+      i++;
+      if(i == n){
+        return 1;
+      }
+      x -= A[i];
+    }
+  }
+  return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +26,19 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+
+  printf("%d\n", ub);
 
   return 0;
 }
