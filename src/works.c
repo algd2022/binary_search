dcd268ae0853bf1d89b8076c8674ab2dac07dd1a
kitamuratomohiro <kitamura.t.ae@m.titech.ac.jp>
2022-06-27 01:43:32 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,24 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int i;
+  int a;
+  int count = 1;
+  int sum = 0;
+  for(i = 0; i < n ; i ++){
+    a = A[i];
+    if(A[i] > m){
+      return 0;
+    }
+    if(sum + A[i] > m){
+     sum = 0;
+     count = count + 1;
+    }
+    sum = sum + A[i];
+  }
+  return count <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +29,20 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1e9;
 
+  while(ub - lb > 1){
+    int mid = (lb + ub)/2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+
+  printf("%d\n", ub);
 
   return 0;
 }
