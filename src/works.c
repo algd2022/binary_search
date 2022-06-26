6e95a5f9ff321e5685d610c3eb537a9ce3879a39
grkon <yo.for.user@gmail.com>
2022-06-27 00:45:39 +0900

    complete works.c

--- src/works.c
+++ src/works.c
@@ -4,6 +4,44 @@ int n;
 int k;
 int A[100000];
 
+int sum()
+{
+  int ret = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    ret += A[i];
+  }
+
+  return ret;
+}
+
+int p(int x)
+{
+  int num = 0;
+  int work = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    work += A[i];
+    if (work > x)
+    {
+      num++;
+      // exception
+      if (A[i] > x)
+      {
+        return 0;
+      }
+      else
+      {
+        work = A[i];
+      }
+    }
+  }
+
+  return (num < k);
+}
+
 int main()
 {
   int i, lb, ub;
@@ -12,6 +50,23 @@ int main()
   {
     scanf("%d", &A[i]);
   }
+
+  lb = 0;
+  ub = sum();
+
+  int mid = (lb + ub) / 2;
+
+  while (ub - lb > 1)
+  {
+    if (p(mid))
+      ub = mid;
+    else
+      lb = mid;
+
+    mid = (lb + ub) / 2;
+  }
+
+  printf("%d", ub);
 
   return 0;
 }
