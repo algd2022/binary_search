33fa77ed1df6794761de52868d9b5f5a9fed01d6
Tomosuke Ichioka <ichioka.t.aa@m.titech.ac.jp>
2022-06-26 20:26:03 +0900

    Submission

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int x){
+  unsigned int sum = 0;
+  for(int i=0;i<n;i++){
+    sum = sum + (A[i] + x - 1) / x;
+  }
+  return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +19,14 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)) ub = mid;
+    else lb = mid;
+  }
+  printf("%d\n",ub);
 
   return 0;
 }
