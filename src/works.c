f567744b5242f1988458678c5237e9800e759961
calliope <calliope183@gmail.com>
2022-06-26 19:47:16 +0900

    implement `works.c`

--- src/works.c
+++ src/works.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 
-int n;
-int k;
-int A[100000];
+unsigned int n, k;
+unsigned int A[100000];
 
-
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
+unsigned int p(unsigned int x) {
+  unsigned int divs = 1;
+  unsigned int sum = 0;
+  for(unsigned int i = 0; i < n; i++) {
+    if(sum + A[i] <= x) {
+      sum += A[i];
+    } else {
+      divs += 1;
+      sum = A[i];
+    }
   }
+  return divs;
+}
 
+int main() {
+  unsigned int lb = 0;
+  unsigned int ub = 1000000001;
+
+  scanf("%d%d", &n, &k);
+  for(unsigned int i = 0; i < n; i++){
+    scanf("%d", &A[i]);
+    if(A[i] > lb) {
+      lb = A[i];
+    }
+  }
+  lb -= 1;
+
+  while(ub - lb > 1) {
+    unsigned int mid = (lb + ub) / 2;
+    if(p(mid) <= k) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
