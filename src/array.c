4d3128d2424bf803d43b51320b421e0377004aec
Kota Yasuhara <yasuhara.k.aa@m.titech.ac.jp>
2022-06-26 19:40:57 +0900

    done 1

--- src/array.c
+++ src/array.c
@@ -4,15 +4,28 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  return (x >= k);
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  //n is length of list
+  //k is border
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  lb = -1;
+  ub = n;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(A[mid])){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
