0051b5ad14dff2cb5597adcef6b44b6d3ad6cab7
Ryoto Goto <goto.r.ah@m.titech.ac.jp>
2022-06-28 00:35:51 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int spear = 0;
+  for(int i = 0; i < n; i++){
+	spear += A[i]/x;
+  }
+  return (long long int) spear >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +18,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000 + 1;
+  while(ub - lb > 1){
+	int mid = (ub + lb) / 2;
+	if(p(mid)){
+		lb = mid;
+	} /* lbはp(lb)が成り立つことがわかっている最大の数*/
+	else{
+		ub = mid;
+	};/* ubはp(ub)が成り立たないことがわかっている最小の数*/
+  }
+  printf("%d\n",lb);
   return 0;
 }
