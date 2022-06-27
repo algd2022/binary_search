29fb7e5dcdc234b17c9454feaf67e7991b9cf61f
morikawa tenma <morikawa.t.af@m.titech.ac.jp>
2022-06-28 08:56:40 +0900

    re

--- src/works.c
+++ src/works.c
@@ -4,12 +4,36 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int  b_sum = 0, count = 0;
+    for (int i = 0; i < n) {
+        if (A[i] > x)return 0;
+        if (A[i] + b_sum > x) {
+            b_sum = A[i];
+            count++;
+        }
+        else b_sum += A[i];
+    }
+    if (count < k)return 1;
+    else return 0;
+
+
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+  }
+  lb = 0;
+  ub = 1000000001;
+  while (ub - lb > 1) {
+      int mid = (lb + ub) / 2;
+      if (p(mid)) {
+          ub = mid;
+      }
+      else lb = mid;
   }
 
 
