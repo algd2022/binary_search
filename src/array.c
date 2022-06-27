5942a2d7cbaf4ab1035251aa689aba078056e781
morikawa tenma <morikawa.t.af@m.titech.ac.jp>
2022-06-28 02:38:50 +0900

    reviced

--- src/array.c
+++ src/array.c
@@ -5,14 +5,23 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-
-  return 0;
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+    ub = n;
+    lb = -1;
+    if (A[ub - 1] <= k) {
+        printf("%d\n", n);
+        return 0;
+    }
+    while (ub - lb > 1) {
+        int mid = (ub + lb) / 2;
+        if (A[mid] >= k)ub = mid;
+        else lb = mid;
+    }
+    printf("%d\n", ub);
+    return 0;
 }
