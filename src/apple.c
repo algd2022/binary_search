ba7c763dc1b3ee3cef9092aa07f43158ceef4eae
Takumi_Hirose <taku.h.113@gmail.com>
2022-06-27 22:46:58 +0900

    finish array

--- src/apple.c
+++ src/apple.c
@@ -5,8 +5,9 @@ int k;
 int A[100000];
 
 
+
 int main(){
-  int i, lb, ub;
+  int i, lb=0, ub=n;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
--- src/array.c
+++ src/array.c
@@ -1,18 +1,30 @@
+
 #include <stdio.h>
 
 int n;
 int k;
 int A[100000];
 
+unsigned int binary_search (int lb, int ub, int k){
+while (ub - lb > 1){
+int mid = (lb + ub ) / 2;
+if (A[ mid ] >= k) ub = mid ;
+else lb = mid ;
+}
+return ub ;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+    int i, ub, lb=0;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
-  }
+    }
+    ub=n;
+    int res=0;
+    res = binary_search(lb, ub, k);
+    printf("%d\n", res);
 
 
-
-  return 0;
+    return 0;
 }
