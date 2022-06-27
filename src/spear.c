52199b5e14f4e016a73f2037522d0ec060a9c81a
Taiga Shimano <shimano.t.ab@m.titech.ac.jp>
2022-06-27 15:13:22 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
-
 int n;
 int k;
 int A[100000];
 
-
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+int p(int n_alt, int array[], int mid_alt, int k_alt) {
+	int spear_number = 0;
+	for (int i = 0; i < n; i++) {
+		spear_number += array[i] / mid_alt;
+	}
+	if (spear_number >= k)return 1;
+	return 0;
 }
+
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	lb = 0;
+	ub = 1000000000;
+	while (ub - lb > 1) {
+		int mid = (lb + ub) / 2;
+		if (p(n, A, mid, k)) lb = mid;
+		else ub = mid;
+	}
+	printf("%d\n", lb);
+	return 0;
+}
+
