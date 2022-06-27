fdff2013364c8bbae78432d347aa6441006d0f5d
Kosuke Otsuru <k.tsuru0412@gmail.com>
2022-06-27 12:04:21 +0900

    reviced

--- src/array.c
+++ src/array.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+unsigned int binary_search(int A[], int x, unsigned int l, unsigned int r){
+  unsigned int m;
+  if(x > A[r]) return r+1;
+  if(l == r) return r;
+  m = (l + r)/2;
+  if(A[m] >= x) return binary_search(A, x, l, m);
+  else return binary_search(A, x, m+1, r);
+}
 
 int main(){
   int i, lb, ub;
@@ -11,8 +19,8 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  lb = 0;
+  ub = n-1;
+  printf("%u", binary_search(A, k, lb, ub));
   return 0;
 }
