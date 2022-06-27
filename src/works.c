cbc88e6adc5b397f73994f8888ab12dd4926b322
Takumi Ueno <ueno.t.au@titech.ac.jp>
2022-06-28 05:05:03 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -3,6 +3,7 @@
 int n;
 int k;
 int A[100000];
+int p(int x);
 
 
 int main(){
@@ -12,6 +13,38 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 1;
+  ub = 1000000000;
+  while(ub-lb>1){
+    int mid = (lb+ub)/2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
 
+  printf("%d\n",ub);
   return 0;
+}
+
+int p(int x){
+  int people=k,time=x,i;
+  for(i=0;i<n&&people>0;){
+    if(A[i]<=time){
+      time-=A[i];
+      i++;
+    }
+    else{
+      people--;
+      time=x;
+    }
+  }
+  if(i==n&&people>=0){
+    return 1;
+  }
+  else{
+    return 0;
+  }
 }
