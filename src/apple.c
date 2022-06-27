70d16506d896c34060570628e5f83d8c71b14a26
gray-1to <graystin88@gmail.com>
2022-06-27 18:56:08 +0900

    8.1,8.2クリア

--- src/apple.c
+++ src/apple.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
 int n;
 int k;
 int A[100000];
 
+bool enough(int x){
+  int sum = 0;
+  for(int j = 0; j < n; j++){
+    sum += ceil((double)A[j]/(double)x);
+  }
+  if(sum <= k){
+    return true;
+  }else{
+    return false;
+  }
+}
 
 int main(){
-  int i, lb, ub;
+  int i, lb=0, ub=pow(10, 9), mid;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
 
+  while(ub - lb > 1){
+    mid = (ub + lb) / 2;
+    if(enough(mid)){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
--- src/array.c
+++ src/array.c
@@ -4,13 +4,23 @@ int n;
 int k;
 int A[100000];
 
-
 int main(){
-  int i, lb, ub;
+  int i, lb, ub, now_index;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = n;
+  while(ub - lb > 1){
+    now_index = (lb + ub) / 2;
+    if(A[now_index] > k){
+      ub = now_index;
+    }else{
+      lb = now_index;
+    }
+  }
+  printf("%d\n", ub);
 
 
 
