ea174c05a1127d0ecf89c7de5f85022cccf79a31
Minami Hayashi <hayashi.m.az@m.titech.ac.jp>
2022-06-28 05:11:20 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+unsigned int binary_search(int a[], int x, unsigned int l, unsigned int r){
+    unsigned int m;
+    if (l == r) return r;
+    if (x > A[r]) return r+1;
+    m = (l + r) / 2;
+    if (A[m] >= x) return binary_search(A, x, l,m);
+    else return binary_search(A, x, m + 1, r);
+    }
 
 int main(){
   int i, lb, ub;
@@ -12,6 +20,8 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-
-  return 0;
+    lb = 0;
+    ub = n - 1;
+    printf("%u", binary_search(A, k, lb, ub));
+    return 0;
 }
