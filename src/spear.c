b736e949bee5a58d5f6e5e24dc7da23fb3d78293
Kosuke Otsuru <k.tsuru0412@gmail.com>
2022-06-27 12:07:11 +0900

    reviced

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,18 @@ int n;
 int k;
 int A[100000];
 
+int spear(int A[], int n, int k, int x){
+  int i, count = 0, nokori;
+  for(i=0; i<n; i++){
+    nokori = A[i];
+    while(nokori - x >= 0){
+      nokori-=x;
+      count++;
+    }
+  }
+  if(count >= k) return 0;
+  return 1;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +24,19 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub)/2;
+    if(spear(A, n, k, mid)){
+    ub = mid;
+    }
+    else{
+    lb = mid;
+    }
+  }
+
+  printf("%u", lb);
 
   return 0;
 }
