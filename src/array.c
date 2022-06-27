6cc348c1a62798cceea3a527e99750c77d13c62b
Kenta00 <tsfx2bifd@i.softbank.jp>
2022-06-28 00:27:54 +0900

    array.c

--- src/array.c
+++ src/array.c
@@ -11,8 +11,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  // 資料より
+  lb = -1;
+  ub = n;
+  while(ub  > 1 + lb){
+    int mid = (ub + lb) / 2;
+    if(A[mid] >= k){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
+  
   return 0;
 }
