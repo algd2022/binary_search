1743c1bcae0f1f7feadc128865c537601d59cd13
chen long <chenlong93@foxmail.com>
2022-06-28 01:22:31 +0800

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,14 +3,40 @@
 int n;
 int k;
 int A[100000];
+int max = 1;
 
-
+int p(int m){
+  	int i,a = 0;
+  	for(i = 0; i < n; i++){
+  		a = A[i] / m + a;
+	  }
+	return a < k;
+  }
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+    if(A[i] > max){
+    	max = A[i];
+	}
   }
+  lb = 0;
+  ub = max;
+  
+  
+  
+  
+  while(ub - lb > 1){
+  	int mid = (lb + ub) / 2;
+  	if(p(mid)){
+  		ub = mid;
+	  }
+	else{
+		lb = mid;
+		}
+	}
+	printf("%d\n",lb);
 
 
   return 0;
