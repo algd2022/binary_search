03d018aa83467672af07b4cf47f6962d55262a68
Takumi Ueno <ueno.t.au@titech.ac.jp>
2022-06-28 05:04:53 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,6 +3,7 @@
 int n;
 int k;
 int A[100000];
+int p(int x);
 
 
 int main(){
@@ -12,6 +13,31 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+  while(ub-lb>1){
+    int mid = (lb+ub)/2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
 
+  printf("%d\n",lb);
   return 0;
+}
+
+int p(int x){
+  int sum=0;
+  for(int i=0;i<n;i++){
+    sum += A[i]/x;
+  }
+  if(sum>=k){
+    return 0;
+  }
+  else{
+    return 1;
+  }
 }
