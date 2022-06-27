c88a827f76972e3cfce88df9df8350e2c5011f3d
Hiroaki Nagata <nagata.h.ad@m.titech.ac.jp>
2022-06-28 03:14:59 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -5,13 +5,30 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+int main() {
+    int i, lb, ub;
+    lb = 0;
+    ub = 1e9;
+    scanf("%d%d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+    while (ub-lb>1) {
+        int B = 0;
+        int mid = (lb + ub) / 2;
+        for (i = 0; i < n; i++) {
+            B = B + A[i] / mid;
+        }
+        if (B<k) {
+            ub = mid;
+        }
+        else {
+            lb = mid;
+        }
+        
+    }
+    printf("%d\n", lb);
 
-
-  return 0;
+    
+    return 0;
 }
