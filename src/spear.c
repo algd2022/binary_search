c3ab01565db478bc138fdda6ad0ccdb6fa21343f
Minami Hayashi <hayashi.m.az@m.titech.ac.jp>
2022-06-28 05:25:15 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,16 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+    int n = 0||1;
+    int i;
+    int m;
+    for (i = 0; i < m; i++){
+        n = n + (A[i] + x - 1);
+}
+
+    return k<= x;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +22,17 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+    lb = 0;
+    ub = A[i] + 1;
+        while (ub -lb > 1) {
+            int mid = (ub + lb) / 2;
+            if (p(mid)){lb = mid;
+                
+            }
+            else {ub = mid;
+                
+            }
+            }
 
   return 0;
 }
