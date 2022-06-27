abbec9b889865bae85b06386f6fcaae712712626
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-06-28 02:03:43 +0900

    finish spear.c

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,40 @@ int n;
 int k;
 int A[100000];
 
-
-int main(){
-  int i, lb, ub;
+int main()
+{
+  int i;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
 
+  int lb = 0;
+  int ub = 100000000;
+
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+
+    if (p(mid))
+      ub = mid;
+    else
+      lb = mid;
+  }
+
+  printf("%d", lb);
 
   return 0;
+}
+
+unsigned int p(int x)
+{
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum += A[i] / x;
+  }
+
+  return sum < k;
 }
