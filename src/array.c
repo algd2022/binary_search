b8ffcd13a3252eb27a8018f3c5b1ae6a0c9d3eeb
chen long <chenlong93@foxmail.com>
2022-06-28 01:21:55 +0800

    revised

--- src/array.c
+++ src/array.c
@@ -5,14 +5,25 @@ int k;
 int A[100000];
 
 
+
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  ub = n;
+  lb = 0;
+  while(ub - lb > 1){
+  	int mid = (lb + ub) / 2;
+  	if(A[mid] >= k){
+  		ub = mid;
+	  }
+	  else{
+	  	lb = mid;
+	  }
+  }  
+  printf("%d\n",ub);
   return 0;
 }
