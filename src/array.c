160b00b53b3eb1b1743ee666e5d115ca79b15e71
reiya0104 <reiya0104@ezweb.ne.jp>
2022-06-28 02:25:23 +0900

    feat: 8.1 配列の二分探索 : `array.c` を実装

--- src/array.c
+++ src/array.c
@@ -4,12 +4,37 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) { return A[m] >= k; }
+
+int binary_search(int lb, int ub) {
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
+  return ub;
+}
+
 int main() {
   int i, lb, ub;
+
+  // 入力
   scanf("%d%d", &n, &k);
   for (i = 0; i < n; i++) {
     scanf("%d", &A[i]);
   }
+
+  // 計算
+  lb = -1;
+  ub = n;
+
+  int x = binary_search(lb, ub);
+
+  // 出力
+  printf("%d\n", x);
 
   return 0;
 }
