0cc8b0a30eb2f3d35824d20d11b8358159c02ab1
Karyu Hase <karyu.ad1ym.2020@gmail.com>
2022-06-28 04:41:23 +0900

    REVISED

--- src/spear.c
+++ src/spear.c
@@ -5,13 +5,45 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+int p(int x) {
+    int m = 0;
+    for (int i = 0; i < n; i++)
+    {
+        m = m + A[i] / x;
+
+    }
+    if (m >= k)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 
-  return 0;
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    lb = 0;
+    ub = 1000000000;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (p(mid)) {
+            lb = mid;
+        }
+        else
+        {
+            ub = mid;
+        }
+    }
+
+    printf("%d", lb);
+
+    return 0;
 }
