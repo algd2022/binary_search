799e1f3d063a4076b1b45011fa8df81b7f03192e
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-06-28 01:11:28 +0900

    finish array.c

--- src/array.c
+++ src/array.c
@@ -4,15 +4,30 @@ int n;
 int k;
 int A[100000];
 
-
-int main(){
-  int i, lb, ub;
+int main()
+{
+  int i;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
 
+  int lb = -1;
+  int ub = n;
 
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+
+    if (A[mid] >= k)
+      ub = mid;
+
+    else
+      lb = mid;
+  }
+
+  printf("%d", ub);
 
   return 0;
 }
