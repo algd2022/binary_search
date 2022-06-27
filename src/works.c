553be3e83342c909c066a7e8dfa98eee24e90f22
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2022-06-27 17:30:51 +0900

    works.cを実装した

--- src/works.c
+++ src/works.c
@@ -1,17 +1,55 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int n;
 int k;
 int A[100000];
 
+bool is_able_assign(int m) {
+  int working_time = 0;
+  int worker_id = 1;
 
-int main(){
+  for (int i = 0; i < n; i++) {
+    working_time += A[i];
+    // 許容量を超過したら次の人に割り振る
+    if (working_time > m) {
+      // 単一の仕事量が設定した許容量より大きかったらfalseを返す
+      if (A[i] > m) {
+        return false;
+      }
+
+      // 人数超過したらfalseを返す
+      if (++worker_id > k) {
+        return false;
+      }
+      working_time = A[i];
+    }
+  }
+
+  return true;
+}
+
+int main() {
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++) {
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+
+  while (ub > lb + 1) {
+    int m = (lb + ub) / 2;
+
+    if (is_able_assign(m)) {
+      ub = m;
+    } else {
+      lb = m;
+    }
+  }
+
+  printf("%d\n", ub);
 
   return 0;
 }
