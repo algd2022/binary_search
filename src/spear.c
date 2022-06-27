72c2b93b9cbc8b8f0a3aa2f428684764c18accdc
bfiab <yuto.o1213@gmail.com>
2022-06-27 16:46:20 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(int i, int x){
+ if(i == 0){
+      return A[0] / x;
+    }
+ else {
+     return p(i - 1, x) + A[i] / x;
+  }
+}
+  
 
 int main(){
   int i, lb, ub;
@@ -11,7 +20,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+ lb = 0;
+ ub = 100001;
+ while(ub - lb > 1){
+     int mid = (ub + lb) / 2;
+     if(p(n, mid) >= k){
+         lb = mid;
+      }
+     else {
+         ub = mid;
+      }
+    }
+  printf("%d\n", lb);
   return 0;
 }
