4ff705c4d1ca3ca5ce1a93ed0baffaa56d57fd0e
Taiyo Kanazawa <kokukoku.nanashi@gmail.com>
2022-06-28 02:59:08 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int i;
+    int s = 0;
+    for (i = 0; i < n; i++) {
+        s = s + A[i] / x;
+    }
+    return s < k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while (ub - lb > 1) {
+      int mid = (lb + ub) / 2;
+      if (p(mid)) {
+          ub = mid;
+      }
+      else {
+          lb = mid;
+      }
+  }
+  printf("%d\n", lb);
   return 0;
 }
