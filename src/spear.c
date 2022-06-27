84173b959f155f61e96b8f5931dc3d338f03b1d7
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2022-06-27 17:11:01 +0900

    spear.cを実装した

--- src/spear.c
+++ src/spear.c
@@ -1,17 +1,48 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int n;
 int k;
 int A[100000];
 
+bool is_able_length(int m) {
+  if (m <= 0) {
+    return true;
+  }
 
-int main(){
+  int sum_spear = 0;
+
+  for (int i = 0; i < n; i++) {
+    sum_spear += A[i] / m;
+
+    if (sum_spear >= k) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int main() {
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++) {
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000001;
+
+  while (ub > lb + 1) {
+    int m = (lb + ub) / 2;
+
+    if (is_able_length(m)) {
+      lb = m;
+    } else {
+      ub = m;
+    }
+  }
+
+  printf("%d\n", lb);
 
   return 0;
 }
