9c831656a2e80d93c9946457a759fb5c8bbfe9fc
annin3 <shunta.titech@gmail.com>
2022-06-28 00:42:47 +0900

    8.2

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,16 @@ int n;
 int k;
 int A[100000];
 
+// x個入るバッグをk個配ると全員がりんごを持って帰れるかどうが
+unsigned int p(int x){
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    //必要なバッグの合計を計算する
+    sum += (A[i] + x -1)/x;
+  }
+  //持って帰れるならtrue
+  return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +22,17 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+      if(p(mid)) {
+        ub = mid;
+      } else {
+        lb = mid;
+      }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
