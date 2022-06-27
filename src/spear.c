869b0df8cf18bb7a70a064e928f68df7db94f632
WataruOtsuki <wtr.otsuki@gmail.com>
2022-06-28 01:11:54 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,43 @@ int n;
 int k;
 int A[100000];
 
+int p(int x)
+{
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum += A[i] / x;
+  }
+  if (sum >= k)
+  {
+    return 1;
+  }
+  else
+    return 0;
+}
 
-int main(){
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
+
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+    if (p(mid) == 1)
+      lb = mid;
+    else
+      ub = mid;
+  }
+
+  printf("%d\n", lb);
 
   return 0;
 }
