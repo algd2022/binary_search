931ac8783d49773bf08ae6bcadb0d138d5d2896e
KumanoAyane <kumano.a.aa@m.titech.ac.jp>
2022-06-27 15:22:26 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -1,17 +1,63 @@
 #include <stdio.h>
 
-int n;
-int k;
-int A[100000];
+  int n;
+  int k;
+  int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
+
+
+  int main() {
+      int i, lb, ub;
+      scanf("%d%d", &n, &k);
+      for (i = 0; i < n; i++) {
+          scanf("%d", &A[i]);
+      }
+
+      int x = 1;
+      while (1) {
+          int s = 0;
+          int m = 1;
+          int o = 0;
+          while (1) {
+
+              lb = -1;
+              ub = n;
+
+              while (ub - lb > 1) {
+                  int mid = (lb + ub) / 2;
+                  if (A[mid] >= x * m) {
+                      ub = mid;
+                  }
+                  else {
+                      lb = mid;
+                  }
+              }
+              s = s + (m-1) * (ub - o);
+              o = ub;
+              if (ub == n) {
+                  break;
+              }
+              else {
+                  m = m + 1;
+              }
+
+          }
+
+          printf("s=%d\n", s);
+
+          if (s < k) {
+              x = x-1;
+              break;
+          }
+          else {
+              x = x + 1;
+          }
+      }
+
+
+
+      printf("%d\n", x);
+
+      return 0;
   }
-
-
-  return 0;
-}
