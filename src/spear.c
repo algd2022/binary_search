9ccb38fbead1138472b6a9faef24f40e0096d030
annin3 <shunta.titech@gmail.com>
2022-06-28 00:52:18 +0900

    8.3

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,16 @@ int n;
 int k;
 int A[100000];
 
+// 長さxの槍をk本作れるかどうか
+int p(int x){
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    //作れる本数の合計を計算する
+    sum += A[i]/x;
+  }
+  //作れるならtrue
+  return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +22,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000001;
+
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)) {
+      lb = mid;
+    } else {
+      ub = mid;
+    }
+  }
+  printf("%d\n", lb);
 
   return 0;
 }
