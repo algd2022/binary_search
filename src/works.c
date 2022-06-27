7cda9050cfff76bb3cc550c647b9d1fcd61b8d57
Takei Hironobu <107602049+TakeiHironobu@users.noreply.github.com>
2022-06-28 08:09:52 +0900

    Solve 8.3 and 8.4

--- src/works.c
+++ src/works.c
@@ -4,6 +4,22 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int i, works, people;
+  works = 0;
+  people = 1;
+  for(i = 0; i < n; i++){
+    if(A[i] > m){
+      return 0;
+    }
+    works += A[i];
+    if(works > m){
+      works = A[i];
+      people += 1;
+    }
+  }
+  return people <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +27,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
