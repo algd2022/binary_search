2187152bab2524da73036b25f9823d632aaf807d
Yuki Kinoshita <kinoshita.y.as@m.titech.ac.jp>
2022-06-27 23:32:04 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,19 @@ int n;
 int k;
 int A[100000];
 
+int p(unsigned int x){
+ int h = 0;
+ int sum = 0;
+ for(int i = 0 ; i < n ; i++){
+  sum += A[i];
+  if(A[i] > x) return 0;
+  else if(sum > x){
+   h++;
+   sum = A[i];
+  }
+ }
+ return h < k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +25,22 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000;
 
+  while(ub - lb > 1){
+
+   int mid = (lb + ub) / 2;
+   
+   if(p(mid)){
+    ub = mid;
+   }
+
+   else{
+    lb = mid;
+   }
+  }
+
+  printf("%d\n" , ub);
   return 0;
 }
