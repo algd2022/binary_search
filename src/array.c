a1bc99bd5d08ff68ccd0635cd6accc25ee036a9b
Kosei Maeda <mae_kou0812@yahoo.cp.jp>
2022-06-27 16:53:41 +0900

    8.1 done

--- src/array.c
+++ src/array.c
@@ -11,8 +11,13 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  lb = -1; //lbはA[lb] < x がわかっている最大の整数
+  ub = n; //ubはA[ub] >=x がわかっている最小の整数
+  while(ub - lb > 1){ 
+    int mid = (lb + ub) / 2;
+    if(A[mid] >= k) ub = mid; //真ん中の値をkと比較して、lbまたはubをmidまで寄せる
+    else lb = mid;
+  }
+  printf("%d\n",ub);
   return 0;
 }
