78f5c8a72f56aabf53fb8817da17210559907ea0
gray-1to <graystin88@gmail.com>
2022-06-27 20:06:51 +0900

    8.4クリア

--- src/works.c
+++ src/works.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
 
 int n;
 int k;
 int A[100000];
 
+bool can(int x){
+  int need = 1, sum = 0;
+  for(int j=0; j < n; j++){
+    if(A[j] > x){
+      return false;
+    }else if(sum + A[j] > x){
+      need++;
+      sum = 0;
+    }
+    sum += A[j];
+  }
+
+  if(need <= k){
+    return true;
+  }else{
+    return false;
+  }
+}
 
 int main(){
-  int i, lb, ub;
+  int i, lb=0, ub=pow(10,4), mid;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  while(ub - lb > 1){
+    mid = (ub + lb) / 2;
+    if(can(mid)){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
