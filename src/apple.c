c3160de7fb71c122143acf00e8959e0b1921d318
showtamu <107395142+showtamu@users.noreply.github.com>
2022-06-28 00:43:35 +0900

    Update apple.c

--- src/apple.c
+++ src/apple.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
-
 int n;
 int k;
 int A[100000];
 
+int Max(int *A){
+  int M = 0;
+  for(int i = 0; i < n; i++){
+    if( M < A[i] ){
+      M = A[i];
+    }
+  }
+  return M;
+}
+
+int p(int x){
+  int total = 0;
+  for(int i = 0; i < n; i++){
+    total += ( A[i] + x - 1 ) / x;
+  }
+  if (total <= k){
+    return 1;
+  }
+  return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -13,5 +32,17 @@ int main(){
   }
 
 
+  lb = 0;
+  ub = Max(A);
+  while (ub-lb>1){
+    int mid = (ub+lb)/2;
+    if (p(mid)==1){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
