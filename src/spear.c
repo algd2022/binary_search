2a168e2840151e1d3546abbb40ae0f6d61b0fdc4
Tatsuya Ito <ito.t.cb@m.titech.ac.jp>
2022-06-28 05:07:20 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += A[i] / x;
+    }
+    return k <= sum;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +18,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 100000001;
+  ub = 0;
+  while (lb - ub > 1) {
+      int mid = (lb + ub) / 2;
+      if (p(mid)) {
+          ub = mid;
+      }
+      else {
+          lb = mid;
+      }
+  }
+  printf("%d\n", ub);
 
 
   return 0;
