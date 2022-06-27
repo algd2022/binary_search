1736fd3c89c3ef3f6d5bd173eeaf2b1937f79956
Koshiro Aoki <aoki.k.as@m.titech.ac.jp>
2022-06-27 21:47:36 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -11,7 +11,28 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 0;
+  for(i = 0; i < n ;i++){
+    ub = ub + A[i];
+  }
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    int real_bags = 0;
+    for(i = 0; i < n; i++){
+      if(A[i] % mid == 0){
+        real_bags = real_bags + A[i] / mid;
+      }else{
+        real_bags = real_bags + A[i] / mid + 1;
+      }
+    }
+    if(real_bags <= k){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
--- src/array.c
+++ src/array.c
@@ -7,12 +7,21 @@ int A[100000];
 
 int main(){
   int i, lb, ub;
-  scanf("%d%d", &n, &k);
+  scanf("%d %d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = n;
+  while(ub - lb > 1){
+    int mid = (ub + lb) / 2;
+    if(A[mid] >= k){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
