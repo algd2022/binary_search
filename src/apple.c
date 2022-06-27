93d1282c75fb5026a17ec86dd17fee5184caa231
Naoki Tokuhiro <naoki@tokuhiro.net>
2022-06-28 05:01:31 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-
 int n;
 int k;
 int A[100000];
 
+
+int p(int x) {
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+        s += (A[i] + x - 1) / x;
+    }
+    if (s <= k) return 1;
+    else return 0;
+}
 
 int main() {
     int i, lb, ub;
@@ -11,11 +19,12 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &A[i]);
     }
-    lb = -1;
-    ub = n;
+
+    lb = 0;
+    ub = 1000000001;
     while (ub - lb > 1) {
-        int mid = (lb + ub) / 2;
-        if (A[mid] >= k) ub = mid;
+        int mid = (ub + lb) / 2;
+        if (p(mid)) ub = mid;
         else lb = mid;
     }
     printf("%d\n", ub);
