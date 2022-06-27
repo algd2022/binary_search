920a19f6c94b2c418d12c2ed9808c53dd056efa7
Ryudai Yokokawa <yokokawa.r.aa@m.titech.ac.jp>
2022-06-28 01:58:56 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int sum = 0;
+  for(int i = 0; i < n; i++){
+    sum += (A[i] + x - 1) / x;
+  }
+  return k >= sum;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +19,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1e9;
+  while(ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if(p(mid)) {
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+  printf("%d\n" , ub);
 
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,23 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){ // xは最大仕事量
+  int workers = 1;
+  int amt = 0;
+  for(int i = 0; i < n; i++){
+    if(A[i] > x){
+      return 0;
+    }
+    else if(amt + A[i] > x){
+      workers++;
+      amt = A[i];
+    }
+    else{
+      amt += A[i];
+    }
+  }
+  return k >= workers;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +29,18 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1e9;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n" , ub);
 
   return 0;
 }
