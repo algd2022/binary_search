a5811c8963ce5156e295ba209d72674008e5bc10
Kenta <tsfx2bifd@i.softbank.jp>
2022-06-28 00:55:01 +0900

    apple.c

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,20 @@ int n;
 int k;
 int A[100000];
 
+//sizeでかばんk個が足りるか確認する関数
+int bag(int size)
+{
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum += (A[i] + size - 1) / size;
+  }
+  if (sum <= k)
+  {
+    return 1;
+  }
+  return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +25,16 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1000000000;
+  // 関数bagをつかって二分探索
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(bag(mid)) ub = mid;
+    else lb = mid;
+  }
 
+  printf("%d\n", ub);
 
   return 0;
 }
