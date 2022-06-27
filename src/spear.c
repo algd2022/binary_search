832db45be3372366d13d963274ca37845e9d1644
Ryuji Matsuo <mryu8212@gmail.com>
2022-06-27 11:26:15 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int sum = 0;
+  for (int i = 0; i < n; i++){
+    sum += A[i] / x;
+  }
+  return sum < k; /*sumは切り捨てられた値であり、求める最小値より小さいxで 　　　　　　　　
+                    sum = k となることがあるため sum = k は偽にしておく*/
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +20,15 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000001; /*lb = 10^9となることがあるため、ub = 10^9 +1 とする*/
+  while (ub - lb > 1) {
+     int mid = (ub + lb) / 2;
+     if (p(mid))  ub = mid;
+     else lb = mid;
+   }
+   printf("%d\n", lb); /*sum = k を偽にしたことにより、
+   　　　　　　　　　　　求めたい最小値はlbに代入されているためlbを出力する*/
 
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,21 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int i, num, sum; /*仕事する人の番号num, 一人の仕事量の合計sum */
+  sum = 0;
+  num = 1;
+  for(i = 0; i < n; i++){
+    if(A[i] <= x) sum += A[i];
+    else return 0; /*xはA[i]の最大値以上であるため、x < A[i] のとき return 0 */
+    if(sum > x){ /*仕事量の合計がxを超えたとき次の人が仕事を始める*/
+      num ++;
+      sum = A[i];
+    }
+  }
+  if(num <= k) return 1;
+  else return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +27,14 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = 1000000000; /*ΣA[i] <= n * 10^4 <= 10^5 * 10^ 4 = 10^9*/
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)) ub = mid ;
+    else lb = mid ;
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
