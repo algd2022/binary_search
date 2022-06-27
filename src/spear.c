812491042ddb0b06a2d4151c5760ead693041325
ksdkt <takutaku.yoshio.0920@gmail.com>
2022-06-28 00:06:49 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int sum = 0;
+  for(int i = 0; i < n; i++){
+    sum = sum + A[i] / m;
+  }
+  return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +18,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1000000001;
+  while(ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,6 +4,23 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int sum = 0;
+  int num = 1;
+  for(int i = 0; i < n; i++){
+    if(sum + A[i] <= m){
+      sum = sum + A[i];
+    }
+    else if(A[i] <= m){
+      sum = A[i];
+      num = num +1;
+    }
+    else{
+      return 0;
+    }
+  }
+  return num <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +28,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
