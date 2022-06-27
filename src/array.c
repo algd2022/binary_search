a58b1b12378f34729bf04261767e30dacfcfd49b
Naoki Tokuhiro <naoki@tokuhiro.net>
2022-06-28 04:41:26 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -5,14 +5,23 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+int main() {
+    int lb, ub;
+    scanf("%d%d", &n, &k);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
 
-
-
-  return 0;
+    lb = -1; ub = n;
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (A[mid] >= k) {
+            ub = mid;
+        }
+        else {
+            lb = mid;
+        }
+    }
+    printf("%d\n", ub);
+    return 0;
 }
