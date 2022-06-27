54eb7b695d760a526ab0e0814d21038a4046ce9c
Karyu Hase <karyu.ad1ym.2020@gmail.com>
2022-06-28 04:39:50 +0900

    REVISED

--- src/apple.c
+++ src/apple.c
@@ -5,13 +5,46 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+int p(int x) {
+    int S = 0;
+    for (int i = 0; i < n; i++)
+    {
+        S = S + (A[i] + x - 1) / x;
+        
+    }
+
+    if(S <= k)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 
-  return 0;
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    lb = 0;
+    ub = 1000000000;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (p(mid)) {
+            ub = mid;
+         }
+        else 
+        {
+            lb = mid;
+        }
+    }
+
+    printf("%d", ub);
+
+    return 0;
 }
