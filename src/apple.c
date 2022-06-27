25a651bf43f9102ef2bb106612add4368c7ad408
Hiroki Hanma <hirokihanma@gmail.com>
2022-06-28 02:20:54 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,37 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int sum = 0;
+    int i;
+    for (i = 0; i < n; i++) {
+        sum += (A[i] + x - 1) / x;
+    }
+    if (sum <= k)
+    {
+        return 1;
+    }
+    else return 0;
+
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = 0;
+  ub = 1000000000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  while (ub - lb > 1) {
+      int mid = (lb + ub) / 2;
+      if (p(mid)) {
+          ub = mid;
+      }
+      else {
+          lb = mid;
+      }
+  }
+  printf("%d/n", ub);
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,34 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int m = 0;
+    int i;
+    for (i = 0; i < n; i++) {
+        m += (A[i]) / x;
+    }
+    if (m >= k) {
+        return 1; 
+    }
+     else 
+        return 0;
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = 0;
+  ub = 1000000000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  while (ub - lb > 1) {
+      int mid = (lb + ub) / 2;
+      if (p(mid)==0) {
+          ub = mid;
+      }
+      else  lb = mid;
+  }
+  printf("%d/n", lb);
   return 0;
 }
