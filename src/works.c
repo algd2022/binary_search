d0c283d6fd22babb00ff584b24c5e0517d9624b4
Shogo Nishimura <nishimurascience@gmail.com>
2022-06-27 14:18:18 +0900

    finally_done!

--- src/works.c
+++ src/works.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-int n;
-int k;
-int A[100000];
+int n = 3;
+int k = 2;
+int A[3] = {1, 4, 2};
 
 int can_distribute(int max_work_amount)
 {
     int i = 0;
-    int human = 1;
+    int human = 0;
     int human_work = 0;
     while (i < n) {
-        if (max_work_amount < A[i]) {
-            return 0;
-        }
         if (human_work + A[i] > max_work_amount) {
+            if(max_work_amount < A[i]) {
+                return 0;
+            }
             human++;
-            human_work = 0;
+            human_work = A[i];
         } else {
             human_work += A[i];
         }
         i++;
     }
-    return(human <= k);
+    return(human < k);
 }
 int main()
 {
@@ -33,14 +33,13 @@ int main()
     lb = 0;
     ub = 100000000;
     while (ub - lb > 1) {
-        printf("%d,%d\n", lb, ub);
         int mid = (lb + ub) / 2;
-        if (can_distribute(mid) == 1) {
+        if (can_distribute(mid)) {
             ub = mid;
         } else {
             lb = mid;
         }
     }
-    printf("%d,%d\n",lb,ub);
+    printf("%d\n",ub);
     return 0;
 }
