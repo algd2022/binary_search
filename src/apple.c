2ebc7cc7ea7d28d9d48f1993a6be95e763511dc5
Shunya Oikawa <oikawa.s.ab@m.titech.ac.jp>
2022-06-28 02:01:53 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,19 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int i,sum;
+  sum=0;
+  for (i=0;i<n;i++){
+    sum +=(A[i]+x-1)/x;
+  }
+  if (sum <k){
+    return 0;
+  }
+  else {
+    return 1;
+  }
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +24,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb=0;
+  ub=1000000000;
+  while(ub-lb>1){
+    int mid =(lb +ub/2);
+    if(p(mid)){
+      ub=mid;
+    }
+    else {
+      lb=mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
