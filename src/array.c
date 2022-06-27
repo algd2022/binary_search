a87d8b12fec41792694d602f56d9f028509a4ab8
igari syu <igarisyu@DESKTOP-GSSIGFC.localdomain>
2022-06-28 06:36:00 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -4,15 +4,28 @@ int n;
 int k;
 int A[100000];
 
+unsigned int binary_search(int A[], int x, int n){
+  int lb = -1;
+  int ub = n;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(A[mid] >= x) ub = mid;
+    else lb = mid;
+  }
+  return ub;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+	int i, ub;
+ 	scanf("%d%d", &n, &k);
+	for(i = 0; i < n; i++){
+    	scanf("%d", &A[i]);
+    }
 
-
-
-  return 0;
+	ub = binary_search(A, k, n);
+	
+	printf("%d\n", ub);
+	return 0;
 }
+
+
