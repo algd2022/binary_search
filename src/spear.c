95717ef14d191e65c6d71f415b49bf8f82319e1a
showtamu <107395142+showtamu@users.noreply.github.com>
2022-06-28 03:09:39 +0900

    Update spear.c

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,23 @@ int n;
 int k;
 int A[100000];
 
+int sum(int *A){
+  int s = 0;
+  for(int i = 0; i < n; i++){
+    s += A[i];
+  }
+  return s;
+}
+int p(int x){
+  int total = 0;
+  for(int i = 0; i < n; i++){
+    total += A[i] / x;  
+  }
+  if (total < k){
+    return 1;
+  }
+  return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +28,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = sum(A) / k;
+  while (ub-lb>1){
+    int mid = (ub+lb)/2;
+    if (p(mid)==1){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", lb);
 
   return 0;
 }
