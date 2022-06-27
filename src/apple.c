d0e80d27e977d72cbde8b7f53238c689a4f6febd
Taiyo Kanazawa <kokukoku.nanashi@gmail.com>
2022-06-28 02:22:33 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) {
+    int i;
+    int s = 0;
+    for (i = 0; i < n; i++) {
+        s = s + (A[i] + m - 1) / m;
+    }
+    return s <= k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +20,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while (ub - lb > 1) {
+      int mid = (lb + ub) / 2;
+      if (p(mid)) {
+          ub = mid;
+      }
+      else {
+          lb = mid;
+      }
+  }
+  printf("%d\n", ub);
   return 0;
 }
