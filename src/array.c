7694645964a674f6bd3954a4bac7a0d21728bdfa
bfiab <yuto.o1213@gmail.com>
2022-06-28 01:48:40 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -20,8 +20,8 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-  lb = 0;
-  ub = 100000;
+  lb = -1;
+  ub = n;
   while(ub - lb > 1){
       int mid = (lb + ub) / 2;
       if(sum(n, mid) <= k){
