b328fd0d90aeb23c3f4188ac38f295ae9ff0eb07
Yuto Shibata <shibata.y.ao@m.titech.ac.jp>
2022-06-28 00:22:48 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int i, s = 0;
+  for(i = 0; i < n; i++){
+    s += (A[i] + x - 1) / x;
+  }
+  return s <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +19,17 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
