84a6b2b626e0f6249ed9a6331e38cf76499406ac
rin <odanagi02@gmail.com>
2022-06-28 03:35:56 +0900

    a

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int cnt = 0;
+  for(int i = 0; i < n; i++){
+    cnt += A[i]/m;
+  }
+  if(cnt >= k) return 1;
+  else return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000001;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      lb = mid;
+    }
+    else {
+      ub = mid;
+    }
+  }
+  printf("%d\n",lb);
   return 0;
 }
