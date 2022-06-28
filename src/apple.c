4de609e6cf1478a9ebc3c513d28b5f3fb390a26d
Kaede Teraoka <teraoka.kaede@gmail.com>
2022-06-28 11:55:53 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,42 @@ int n;
 int k;
 int A[100000];
 
+int p(int m)
+{
+  int bag = 0;
+  for (int i = 0; i < n; i++)
+  {
+    bag += (A[i] + m - 1) / m;
+  }
+  if (bag <= k)
+    return 1;
+  else
+    return 0;
+}
 
-int main(){
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
-
+  lb = 0;
+  ub = 1e9;
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+    if (p(mid))
+    {
+      ub = mid;
+    }
+    else
+    {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,41 @@ int n;
 int k;
 int A[100000];
 
+int p(int m)
+{
+  int spear = 0;
+  for (int i = 0; i < n; i++)
+  {
+    spear += A[i] / m;
+  }
+  if (spear < k)
+    return 1;
+  else
+    return 0;
+}
 
-int main(){
+int main()
+{
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++)
+  {
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1e9;
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+    if (p(mid))
+    {
+      ub = mid;
+    }
+    else
+    {
+      lb = mid;
+    }
+  }
+  printf("%d\n", lb);
   return 0;
 }
