385ca94904802ba86e1d82f6a0d58b41d4c02fef
Nana Oizumi <oizumi.n.aa@m.titech.ac.jp>
2022-06-28 11:42:48 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,29 @@ int n;
 int k;
 int A[100000];
 
+int p(int bagapple){
+  int bagsum = 0,i;
+  for(i = 0;i < n;i++){
+    bagsum += (A[i] + bagapple - 1)/bagapple;
+  }
+  return bagsum <= k;
+}
 
 int main(){
   int i, lb, ub;
+  printf("n? k?\n");
   scanf("%d%d", &n, &k);
+  printf("A[i]?\n");
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)) ub = mid;
+    else lb = mid;
+  }
+  printf("x = %d\n",ub);
   return 0;
 }
