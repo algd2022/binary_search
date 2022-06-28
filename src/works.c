9402603b8b888d3ca3aa621a60a9f86b6fe567a0
Shunya Oikawa <oikawa.s.ab@m.titech.ac.jp>
2022-06-28 10:52:49 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,23 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int i,j,sum;
+  sum=0;
+  j=1;
+  for(i=0;i<n;i++){
+    if(A[i]>x)return 0;
+    if(j+A[i]<=x){
+      j+=A[i];
+    }
+    else{
+      j++;
+      sum=A[i];
+    }
+  }
+  return j<=k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +28,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb=0;
+  ub=1000000000;
+  while(ub-lb>1){
+    int mid =(lb +ub/2);
+    if(p(mid)){
+      ub=mid;
+    }
+    else {
+      lb=mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
