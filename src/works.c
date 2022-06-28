1db178806421ecfa1336046943f13522b240c155
rin <odanagi02@gmail.com>
2022-06-28 10:16:05 +0900

    a

--- src/works.c
+++ src/works.c
@@ -4,6 +4,23 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int sum = 0, cnt = 1;
+  for(int i = 0; i < n; i++){
+    if(A[i] > m){
+      return 0;
+    }
+    else if(sum + A[i] <= m){
+      sum += A[i];
+    }
+    else {
+      sum = A[i];
+      cnt++;
+    }
+  }
+  if(cnt <= k) return 1;
+  else return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +28,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
