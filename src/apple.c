b4f59b39ef730715f1b83e3cb513707ac1d1e76d
justinbiebertokyo <li.y.bj@m.titech.ac.jp>
2022-06-28 10:48:07 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int sgm = 0;
+  int i;
+  for (i=0; i < n; i++){
+    sgm += (A[i] + x - 1) / x;
+  }
+  return sgm <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,6 +20,17 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-
+lb = 0;
+ub = 1000000000; 
+while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
-}
+} 
