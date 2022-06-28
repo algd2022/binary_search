cc51aa3f9a4d37639bed65ca7f5d5948ec6b1e99
tetsushi33 <takatetsu12@gmail.com>
2022-06-28 10:52:59 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -1,18 +1,31 @@
+/* 8.1 配列の二分探索 */
+
 #include <stdio.h>
 
-int n;
-int k;
+int n; //配列の要素数
+int k; //比較対象
 int A[100000];
 
-
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+    //ub, lb, A[]の設定
+    int i, lb = -1, ub;
+    scanf("%d%d\n", &n, &k);
+    ub = n;                     
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
 
+    //binary search
+    while(ub - lb > 1){
+        int mid = (lb + ub) / 2;
+        if(A[mid] >= k){
+            ub = mid;
+        }else{
+            lb = mid;
+        }
+    }
 
-
-  return 0;
+    //answer = ub
+    printf("%d\n", ub);
+    return 0;
 }
