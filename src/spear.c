9415461fca3e383b85b66dd696b4d037bcc80d27
Nana Oizumi <oizumi.n.aa@m.titech.ac.jp>
2022-06-28 11:43:27 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,29 @@ int n;
 int k;
 int A[100000];
 
+int p(int cutlong){
+  int spearsum = 0,i;
+  for(i = 0;i < n;i++){
+    spearsum += A[i]/cutlong;
+  }
+  return spearsum >= k;
+}
 
 int main(){
   int i, lb, ub;
+  printf("n? k?\n");
   scanf("%d%d", &n, &k);
+  printf("A[i]?\n");
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000001;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)) lb = mid;
+    else ub = mid;
+  }
+  printf("x = %d\n",lb);
   return 0;
 }
