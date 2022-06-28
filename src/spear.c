3d7e98f484ab61efa4283ba0ad30379a00d90c60
Riku Onozawa <onozawa.r.aa@m.titech.ac.jp>
2022-06-28 11:34:37 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -8,10 +8,25 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for(i = 1; i <= n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1000000000;
+  int siguma;
+  while(ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    for(i = 1; i <=n; i++){
+      siguma = siguma + A[i] / mid;
+    }
+    if(siguma >= k){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
 
-
+  printf("%d\n", ub);
   return 0;
 }
