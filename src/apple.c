51d333d5197dd21171fb844992f13f6ae01a58e6
Naoki Wada <wada.n.ae@m.titech.ac.jp>
2022-06-28 09:38:29 +0900

    ver1

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int i;
+  int p0 = 0;
+  for(i = 0; i < n; i++){
+    p0 = p0 + (A[i] + m - 1) / m;
+  }
+  return p0 <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 0;
+  for(i = 0; i < n; i++){
+    if (A[i] > ub) ub = A[i];
+  }
 
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)) ub = mid;
+    else lb = mid;
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int i;
+  int p0 = 0;
+  for(i = 0; i < n; i++){
+    p0 = p0 + A[i] / m;
+  }
+  return p0 >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +19,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 0;
+  for(i = 0; i < n; i++){
+    if (A[i] > ub) ub = A[i];
+  }
+
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)) lb = mid;
+    else ub = mid;
+  }
+  printf("%d\n", lb);
 
 
   return 0;
