8063d7fa7f3a7219b11bec32098cd8b497168351
uta-cherry <107653412+uta-cherry@users.noreply.github.com>
2022-06-28 11:51:59 +0900

    Add files via upload

--- src/works.c
+++ src/works.c
@@ -4,6 +4,22 @@ int n;
 int k;
 int A[100000];
 
+int p(int m, int k, int *A){
+  int i;
+  int save = m;
+  int p = 0;
+  for (i = 0; i < n; i++){
+    if (m - A[i]>= 0){
+      m -= A[i];
+    }
+    else{
+      m = save-A[i];
+      p++;
+    }
+  }
+  
+  return p <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +27,24 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  int max;
+  max = A[0];
+  for (i = 0; i < n; i++) {
+      if (A[i] > max) {
+          max = A[i];
+      }
+  }
+  lb = max;
+  ub = 1000000001; 
+  while ( ub - lb > 1) {
+    int mid = ( lb + ub ) / 2;
+    if( p ( mid , k , A )){
+      ub = mid ;
+    }
+    else {
+    lb = mid ;
+    }
+  }
+  printf ( "%d\n " , ub );
   return 0;
 }
