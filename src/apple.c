59292c87c3d5d08aabcf95c0ce8d51847f054ce5
Kazuaki Shioda <s.kazuaki0930@gmail.com>
2022-07-02 08:55:34 +0900

    revised2

--- src/apple.c
+++ src/apple.c
@@ -24,7 +24,7 @@ int main(){
 	}
 
 	lb = 0;
-	ub = 1000000001;
+	ub = 1000000000;
 	while(ub - lb > 1){
 		int mid = (ub + lb) / 2;
 		if (p(mid)) {
--- src/works.c
+++ src/works.c
@@ -2,25 +2,21 @@
 
 int n;
 int k;
-int A[10000];
+int A[100000];
 
 int p(int m) {
-	int i, k_allo = 0, m_rem;
+	int i, k_allo = 1, m_rem;
 	m_rem = m;
 	for (i = 0; i < n; i++) {
-		if(m_rem >= A[i]){
+		if(A[i] > m) return 0;
+		else if(m_rem >= A[i]){
 			m_rem -= A[i];
 		}
-		else if(m >= A[i]){
+		else{
 			k_allo += 1;
 			m_rem = m - A[i];
 		}
-		else {
-			k_allo = 100000;
-			break;
-		}
 	}
-	k_allo += 1;
 	return (long long int) k_allo <= k;
 }
 
@@ -32,7 +28,7 @@ int main(){
   }
 
   lb = 0;
-  ub = 1000000001;
+  ub = 1000000000;
   while (ub - lb > 1) {
 	  int mid = (ub + lb) / 2;
 	  if (p(mid)) {
