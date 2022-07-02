7419831fda236a072be031fb0cbedab41eb2bc5f
KojimaIssei <kojima.i.aa@m.titech.ac.jp>
2022-07-02 22:20:16 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,8 +3,6 @@
 int n;
 int k;
 int A[100000];
-int B[100000];
-int x;
 int p(x){
   int i;
   int sum;
@@ -24,9 +22,6 @@ int main(){
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
-  }
-  for(x = 1; x < n; x++){
-    B[x]=p(x);
   }
   lb = 0;
   ub = 1000000000;
