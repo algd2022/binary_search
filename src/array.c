8c2ac356eae4d09929ba0db7c766a675e16e1374
Yuto Makimoto <makimoto.y.aa@m.titech.ac.jp>
2022-07-04 04:15:14 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -11,18 +11,14 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &A[i]);
     }
-    lb = 0;
-    ub = n - 1;
-    int ans;
-    if (A[n - 1] < k)ans = n;
-    else {
-        while (lb != ub) {
-            int cen = (lb + ub) / 2;
-            if (A[cen] < k)lb = cen + 1;
-            else ub = cen;
-        }
-        ans = ub;
+    lb = -1;
+    ub = n;
+    while (lb != ub) {
+        int cen = (lb + ub) / 2;
+        if (A[cen] < k)lb = cen + 1;
+        else ub = cen;
     }
+    int ans = ub;
     printf("%d\n", ans);
     return 0;
 }
