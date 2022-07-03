04e5caa1c88a3f0f983c4bab83072eccd430327d
Jun Miyake <zyun3788105@gmail.com>
2022-07-03 12:43:43 +0900

    修正しました

--- src/apple.c
+++ src/apple.c
@@ -5,45 +5,48 @@ int k;
 int A[100000];
 
 
+int p(int x) {
+    //あるxに対して必要な鞄の数
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += (A[i] + x - 1) / x;
+    }
+
+    if (sum <= k) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 int main(){
     //項数n　と　要素k　の入力
-    printf("input 'n' 'k':\n");
     scanf("%d%d", &n, &k);
 
     //kの入力制約
-    int lb=-1, ub=1;
-    int x=1;
-  
+    int lb=0, ub=1;
+    
+    //数列A[i]を入力
     for(int i = 0; i < n; i++){
-        printf("input number %d/%d :",i+1,n);
         scanf("%d", &A[i]);
         if (ub < A[i])ub = A[i];
     }
-    x = ub;
 
-    printf("input finished\n");
-
-    int m = 0, sum = 0;
+    int mid = 1;
 
     while (ub - lb > 1) {
-
-        //xを設定
-        x = (ub + lb) / 2;
-        sum = 0;
-        //xの時に必要な鞄の数sumを計算
-        for (int i = 0; i < n; i++) {
-            sum += (A[i] + x - 1) / A[i];
+        mid = (ub + lb) / 2;
+        if (p(mid)) {
+            ub = mid;
         }
-        //sum>=kの時ubを減らす、sum<kの時lbを増やす
-        if (sum >= k) lb = x;
-        else ub = x;
-
-        //printf("x:%d,lb:%d,ub:%d,sum:%d\n", x, lb, ub, sum);
-
-        m += 1;
-        if (m > 100) break;
+        else {
+            lb = mid;
+        }
     }
     
-    printf("Answer:%d\n", ub);
+    printf("%d\n", ub);
     return 0;
 }
+
