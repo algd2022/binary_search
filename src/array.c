52c4a65630db69e891d268522ea52a14cce83f6d
tsu-ru <94231841+tsuuuru@users.noreply.github.com>
2022-07-03 22:52:24 +0900

    Update array.c
    再提出しました。
    よろしくお願いします。

--- src/array.c
+++ src/array.c
@@ -4,23 +4,19 @@ int n;
 int k;
 int A[100000];
 
-unsigned int binary_search(int A[], int x, unsigned int l, unsigned int r){
-  unsigned int m;
-  if(x > A[r]) return r+1;
-  if(l == r) return r;
-  m = (l + r)/2;
-  if(A[m] >= x) return binary_search(A, x, l, m);
-  else return binary_search(A, x, m+1, r);
-}
-
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-  lb = 0;
-  ub = n-1;
-  printf("%u", binary_search(A, k, lb, ub));
+  lb = -1;
+  ub = n;
+  while(ub - lb > 1){
+    int mid = (lb + ub)/2;
+    if(A[mid] > k) ub = mid;
+    else lb = mid;
+  }
+  printf("%u", ub);
   return 0;
 }
