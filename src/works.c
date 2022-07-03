fbe768735edaa4ca857ffd997d0051486ff6cae6
Jun Miyake <zyun3788105@gmail.com>
2022-07-03 12:44:34 +0900

    修正しました

--- src/works.c
+++ src/works.c
@@ -4,49 +4,59 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int sum = 0, job = 0;
+
+    //1人x以下の仕事量をしたときに必要な人数を計算
+    for (int i = 0; i < n; i++) {
+
+        if (A[i] > x)return 0;
+
+        job += A[i];
+
+        if (job > x) {
+            sum++;
+            job = A[i];
+        }
+    }
+    sum++;
+
+    if (sum <= k) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 
 int main(){
-    int i, /*lb = 0,*/ ub = 0;
+    int i, lb = 0, ub = 0;
 
     //項数n　と　要素k　の入力
-    printf("input 'n' 'k'\n");
     scanf("%d%d", &n, &k);
 
+    //数列を入力
     for(i = 0; i < n; i++){
-        printf("input number %d/%d :", i + 1, n);
         scanf("%d", &A[i]);
-        if (ub < A[i])ub = A[i];
+        ub += A[i];
+        if (lb < A[i]) lb = A[i];
     }
-    
-    printf("input finished\n");
+    lb--;
 
-    int x = ub, sum = 0;
-    int m = 0;
+    int mid = 1;
 
-    i = 0;
-    for(int j = 0 ; j < k; j++){
-        //i = 0;
-        sum = 0;
+    while (ub - lb > 1) {
+        mid = (ub + lb) / 2;
 
-        printf("j:%d,x:%d\n",j, x);
-
-        while (sum < n) {
-            sum += A[i];
-
-            printf("i:%d,j:%d,sum:%d,k:%d\n", i, j, sum, k);
-
-            m += 1;
-            if (m > 10) {
-                printf("over");
-                break;
-            }
-            i += 1;
-            if (i >= n)break;
+        if (p(mid)) {
+            ub = mid;
         }
-
-        if (x < sum)x = sum;
+        else {
+            lb = mid;
+        }
     }
 
-    printf("Answer:%d\n", x);
+    printf("%d\n", ub);
     return 0;
 }
