cadf30419e627f91d07463305e9edfa870fc6a9b
Konosuke Kakoi <kakoi.kounosuke@gmail.com>
2022-07-03 19:44:15 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -5,40 +5,30 @@ int k;
 int A[100000];
 
 int p(int m){
-  int i, j, l, o, p;
-  int B[k];
-  int C[k];
-  B[k - 1] = 0;
+  int i, j, l, a;
+  j = 0;
+  l = 0;
+  a = 0;
   for(i = 0; i < k - 1; i++){
-    B[i] = A[i];
-    C[i] = 1;
-  }
-  for(j = k - 1; j < n; j++){
-    B[k - 1] = B[k - 1] + A[j];
-  }
-  C[n - k] = n - k + 1;
-
-  for(l = 0; l < k; l++){
-    if(B[l] <= m){
-      if(l == k - 1) return B[l] <= m;
+    a = A[l];
+    if(a > m) return a <= m;
+    for(j = l; ((j < n - k + i) && (a <= m)); j++){
+      a = a + A[j + 1];
     }
-    else {
-      if((C[l] == 1) || (l == 0)) return B[l] <= m;
-      p = 0;
-      for(o = 0; o < l; o++){
-        p = p + C[o];
-      }
-      B[l] = B[l] - A[p];
-      B[l - 1] = B[l - 1] + A[p]; 
-      C[l] = C[l] - 1;
-      C[l - 1] = C[l - 1] + 1;
-      l = l - 2;
+    if((j == n - k + i) && (a <= m)){ 
+      j = j + 1;
     }
+    l = j;
   }
-  return 0;
+  a = A[l];
+  for(i = l; i < n - 1; i++){
+    a = a + A[i + 1];
+  }
+  return a <= m;
 }
 
-/* pの計算量はO(n * (2 ** n)) */
+
+/* pの計算量はO(n) */
 
 int main(){
   int i, lb, ub;
