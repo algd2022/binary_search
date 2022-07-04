b068ad650f45163fade45cb46e8e900750899007
morikawa tenma <morikawa.t.af@m.titech.ac.jp>
2022-07-04 21:30:11 +0900

    teisyutu

--- src/array.c
+++ src/array.c
@@ -11,15 +11,11 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &A[i]);
     }
-    ub = n;
     lb = -1;
-    if (A[ub - 1] <= k) {
-        printf("%d\n", n);
-        return 0;
-    }
+    ub = n;
     while (ub - lb > 1) {
-        int mid = (ub + lb) / 2;
-        if (A[mid] >= k)ub = mid;
+        int mid = (lb + ub) / 2;
+        if (A[mid] >= k) ub = mid;
         else lb = mid;
     }
     printf("%d\n", ub);
--- src/works.c
+++ src/works.c
@@ -6,7 +6,7 @@ int A[100000];
 
 int p(int x) {
     int  b_sum = 0, count = 0;
-    for (int i = 0; i < n) {
+    for (int i = 0; i < n; i++) {
         if (A[i] > x)return 0;
         if (A[i] + b_sum > x) {
             b_sum = A[i];
@@ -20,22 +20,22 @@ int p(int x) {
 
 }
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-  lb = 0;
-  ub = 1000000001;
-  while (ub - lb > 1) {
-      int mid = (lb + ub) / 2;
-      if (p(mid)) {
-          ub = mid;
-      }
-      else lb = mid;
-  }
-
-
-  return 0;
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+    lb = 0;
+    ub = 1000000001;
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (p(mid)) {
+            ub = mid;
+        }
+        else lb = mid;
+    }
+    printf("%d\n", ub);
+    return 0;
 }
+
