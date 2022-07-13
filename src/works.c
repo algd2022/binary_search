6ede82c6dd9577629e3c914c2302fabbc4ed4f81
mina1102 <107454670+mina1102@users.noreply.github.com>
2022-07-13 17:10:36 +0900

    Add files via upload

--- src/works.c
+++ src/works.c
@@ -6,19 +6,21 @@ int A[100000];
 
 int p(int x){
     int sum = 1,p = 0, m = 1;
-    for(int m=0; m < n; m++)
-        if(p + A[m]<=x){p = p + x;
+    for(int i=0; i < n; i++)
+        if(x < A[i]){
+          return 0;
             
         }
-        else if(x < A[m]){m = 0;
+        else if(m + A[i]<= x){
+          m = m + A[i];
             
         }
-        else {p = A[m];
-            sum++;
+        else {
+          m = A[i];
+          sum++;
             
         }
 
-return sum<=k && m ;
 }
 
 int main(){
@@ -26,6 +28,17 @@ int main(){
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+  }
+  lb  = 0;
+  ub = 1000000;
+  while (ub - lb > 1){
+    int mid = (ub + lb) / 2;
+    if (p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
   }
 
 
