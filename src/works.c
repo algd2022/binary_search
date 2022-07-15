5756fe6af877d159a778cffcf237ffaa81f4c8d8
Hiroki Mikagi <mikagi.h.aa@m.titech.ac.jp>
2022-07-15 21:50:27 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -30,16 +30,16 @@ int main(){
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
-    lb = 0;
-    ub = 1e9;
-    while(ub - lb > 1){
-      int mid = (lb + ub) / 2;
-      if(p(mid)){
-        ub = mid;
-      }
-      else {
-        lb = mid;
-      }
+  }
+  lb = 0;
+  ub = 1e9;
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else {
+      lb = mid;
     }
   }
   printf("%d\n", ub);
